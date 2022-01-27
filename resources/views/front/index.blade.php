99a480d3c14e4b837aa680f3b188466c460e9981
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2022-01-27 14:28:27 +0700

    import home view from jejak-lulusan repo

---
+++ resources/views/front/index.blade.php
@@ -0,0 +1,18 @@
+@extends('layout.main')
+
+@section('content')
+
+    <div class="main-container">
+        <div class="left-content">
+            <h1 class="welcome">
+                Selamat Datang <br>
+                di <span class="portal-jejak">Portal Jejak Kelulusan</span> <br>
+                SMAN 1 Rawamerta
+            </h1>
+        </div>
+        <div class="right-content">
+            <img src="/assets/img/graduation.svg" alt="Graduation" class="hero-img">
+        </div>
+    </div>
+
+@endsection

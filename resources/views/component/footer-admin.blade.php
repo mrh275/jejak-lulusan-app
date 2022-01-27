33b3d3936d230c46aa8a98ecee45d46bfc650548
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2022-01-27 22:34:45 +0700

    import footer component for view panel alumni

---
+++ resources/views/component/footer-admin.blade.php
@@ -0,0 +1,4 @@
+<div class="footer">
+    <span>Copyright 2021 &copy; SMAN 1 Rawamerta. All right reserved.</span>
+    <span class="creator">Made with <i class='bx bxs-heart'></i> by Muhamad Ramdani H.</span>
+</div>

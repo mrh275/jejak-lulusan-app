59d95091ae68976025c8e246a29637d6473b583d
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2022-01-27 23:08:26 +0700

    created new method "page" to receive uri segment for handling view page

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -11,8 +11,73 @@ class AdminController extends Controller
 
         $data = [
             'title'     => 'Dashboard',
+            'activeMenu' => 'dashboard',
+            'activeSubMenu' => ''
         ];
 
-        return view('admin.dashboard');
+        return view('admin.dashboard', $data);
     }
+
+    public function dataDiri($page = null)
+    {
+        // Form Biodata handler
+        if ($page == 'biodata') {
+            return $this->biodata();
+        }
+
+        // Form data orang tua handler
+        if ($page == 'data-orangtua') {
+            return $this->dataOrangTua();
+        }
+
+        // if ($page == 'datatable-biodata') {
+        //     return $this->tableBiodata();
+        // }
+        // if ($page == 'datatable-orangtua') {
+        //     return $this->tableOrangTua();
+        // }
+
+        echo "Page not found";
+    }
+
+    public function biodata()
+    {
+        $data = [
+            'title'     => 'Form Biodata',
+            'activeMenu' => 'data-diri',
+            'activeSubMenu' => 'biodata'
+        ];
+
+        return view('admin.biodata', $data);
+    }
+    public function dataOrangTua()
+    {
+        $data = [
+            'title'     => 'Form Data Orang Tua',
+            'activeMenu' => 'data-diri',
+            'activeSubMenu' => 'data-orangtua'
+        ];
+
+        return view('admin.data-orangtua', $data);
+    }
+    // public function tableBiodata()
+    // {
+    //     $data = [
+    //         'title'     => 'Tabel Biodata Lulusan',
+    //         'activeMenu' => 'data-diri',
+    //         'activeSubMenu' => 'tabel-biodata'
+    //     ];
+
+    //     return view('admin.dashboard', $data);
+    // }
+    // public function tableOrangTua()
+    // {
+    //     $data = [
+    //         'title'     => 'Tabel Data Orang Tua',
+    //         'activeMenu' => 'data-diri',
+    //         'activeSubMenu' => 'tabel-orangtua'
+    //     ];
+
+    //     return view('admin.dashboard', $data);
+    // }
 }

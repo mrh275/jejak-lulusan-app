6b633f87bdc7ec2c6d7c1fe237d85f9c015a6fdc
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2022-01-27 14:28:55 +0700

    split navbar element to navbar component

---
+++ resources/views/component/navbar.blade.php
@@ -0,0 +1,33 @@
+<nav class="navbar">
+    <div class="brand-container">
+        <a href="/" class="img-brand-link">
+            <img src="/assets/img/logo.png" alt="Logo" class="img-brand">
+        </a>
+        <a href="/" class="brand-link">
+            SMAN 1 RAWAMERTA
+        </a>
+    </div>
+    <div class="toggle-menu">
+        <button class="cursor-pointer hbButton" id="toggleMenu">
+            <div class="hb-menu bg-gray-400"></div>
+            <div class="hb-menu bg-gray-400"></div>
+            <div class="hb-menu bg-gray-400"></div>
+        </button>
+    </div>
+    <div class="menu-container collapse -left-full" id="menu">
+        <ul class="nav-menu">
+            <li class="nav-list">
+                <a href="/" class="nav-link">Beranda</a>
+            </li>
+            <li class="nav-list">
+                <a href="/daftar-lulusan" class="nav-link">Daftar Lulusan</a>
+            </li>
+            <li class="nav-list">
+                <a href="/pengumuman-kelulusan.html" class="nav-link">Pengumuman</a>
+            </li>
+            <li class="nav-list">
+                <a href="/login.html" class="nav-link">Login</a>
+            </li>
+        </ul>
+    </div>
+</nav>

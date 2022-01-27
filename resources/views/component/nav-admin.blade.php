c191ff70961ae995b56ee2c6dd7466cdf233f4e1
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2022-01-27 22:34:31 +0700

    import navbar component for view panel alumni

---
+++ resources/views/component/nav-admin.blade.php
@@ -0,0 +1,65 @@
+<div class="navbar">
+    <button class="toggle-sidebar">
+        <i class='bx bx-menu'></i>
+    </button>
+    <div class="nav-right">
+        <div class="notification">
+            <button class="btn-notif">
+                <i class='bx bxs-bell'></i>
+                <span class="badges" data-badge="*"></span>
+            </button>
+            <ul class="nav-dropdown">
+                <li class="nav-drop-list">
+                    <a href="#" class="nav-drop-link">
+                        <i class='bx bx-comment-error'></i>
+                        <span class="nav-drop-name">New comments</span>
+                    </a>
+                </li>
+                <hr class="nav-drop-separator">
+                <li class="nav-drop-list">
+                    <a href="#" class="nav-drop-link">
+                        <i class='bx bx-comment-error'></i>
+                        <span class="nav-drop-name">New comments</span>
+                    </a>
+                </li>
+                <hr class="nav-drop-separator">
+                <li class="nav-drop-list">
+                    <a href="#" class="nav-drop-link">
+                        <i class='bx bx-comment-error'></i>
+                        <span class="nav-drop-name">New comments</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+        <div class="nav-user">
+            <button class="btn-user">
+                <i class='bx bxs-user'></i>
+            </button>
+            <ul class="nav-dropdown">
+                <li class="nav-drop-list nav-drop-username">
+                    <span class="nav-drop-name">Administrator</span>
+                </li>
+                <li class="nav-drop-list">
+                    <a href="/edit-akun.html" class="nav-drop-link">
+                        <i class='bx bxs-user'></i>
+                        <span class="nav-drop-name">Profil</span>
+                    </a>
+                </li>
+                <hr class="nav-drop-separator">
+                <li class="nav-drop-list">
+                    <a href="ubah-password.html" class="nav-drop-link">
+                        <i class='bx bx-key'></i>
+                        <span class="nav-drop-name">Ubah password</span>
+                    </a>
+                </li>
+                <hr class="nav-drop-separator">
+                <li class="nav-drop-list">
+                    <a href="/logout.html" class="nav-drop-link">
+                        <i class='bx bx-log-out'></i>
+                        <span class="nav-drop-name">Logout</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>

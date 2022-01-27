63f8234425e365dfd9523bedf27032406d22da7f
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2022-01-28 00:18:15 +0700

    added condition check for some libraries if specified menu active

--- resources/views/layout/admin/main.blade.php
+++ resources/views/layout/admin/main.blade.php
@@ -13,15 +13,24 @@
     <link rel="stylesheet" href="/assets/admin/css/admin.css">
     <link rel="stylesheet" href="/assets/admin/css/page.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <!-- <link rel="stylesheet" href="/dist/css/all.min.css"> -->
     <link href='https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css' rel='stylesheet'>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css" />
+
 
     <!-- Javascript -->
     <script src="/assets/admin/js/jquery.min.js"></script>
-    <script src="https://code.jquery.com/ui/1.13.0/jquery-ui.js"></script>
+    {{-- <script src="https://code.jquery.com/ui/1.13.0/jquery-ui.js"></script> --}}
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-    <script src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+
+    @if ($activeMenu == 'dashboard')
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css" />
+        <script src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+    @endif
+    @if ($activeSubMenu == 'biodata' || 'data-orangtua')
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/vanillajs-datepicker@1.2.0/dist/css/datepicker.min.css">
+        <script src="https://cdn.jsdelivr.net/npm/vanillajs-datepicker@1.2.0/dist/js/datepicker.min.js"></script>
+    @endif
+
+
 
 </head>
 
@@ -53,8 +62,28 @@
                 $('#{{ $activeMenu }}').next().toggleClass('show')
             @endif
         }
+        @if ($activeSubMenu == 'biodata' || 'data-orangtua')
+            $(document).ready(function() {
+            const inputDate = $('input#input-tanggalLahir').on('mousedown', function(){
+            const datepicker = new Datepicker(this, {
+            format: {
+            toValue(date) {
+            const fullYearDate = date.replace(/\/(\d\d)$/, '/20$1');
+            return Datepicker.parseDate(fullYearDate, 'dd/mm/yyyy')
+            },
+            toDisplay(date) {
+            return Datepicker.formatDate(date, 'dd/mm/yyyy');
+            },
+            },
+            autohide: true,
+            });
+            });
+        
+            })
+        @endif
     </script>
     <script src="/assets/admin/js/admin.js"></script>
+
 </body>
 
 </html>

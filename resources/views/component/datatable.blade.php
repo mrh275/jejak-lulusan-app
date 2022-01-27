9e62b0f4aedca614699ff2c35538262b408b84a2
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2022-01-27 22:05:01 +0700

    separating datatable libraries assets

---
+++ resources/views/component/datatable.blade.php
@@ -0,0 +1,5 @@
+<link rel="stylesheet" href="https://cdn.datatables.net/1.11.3/css/jquery.dataTables.min.css">
+
+<!-- JS Assets -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+<script src="https://cdn.datatables.net/1.11.3/js/jquery.dataTables.min.js"></script>

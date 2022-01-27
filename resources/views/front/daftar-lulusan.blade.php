38f301cbd5d9c95c4c7c9c618ba909b2cd87848e
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2022-01-27 14:31:46 +0700

    import daftar lulusan page from jejak-lulusan repo

---
+++ resources/views/front/daftar-lulusan.blade.php
@@ -0,0 +1,250 @@
+@extends('layout.page')
+
+@section('content')
+    <div class="card">
+        <div class="card-header">
+            <h3 class="title">
+                Daftar Lulusan
+            </h3>
+        </div>
+        <div class="card-body">
+            <table id="daftar-lulusan" class="daftar-lulusan stripe">
+                <thead>
+                    <tr>
+                        <td>No</td>
+                        <td>Nama</td>
+                        <td>NISN</td>
+                        <td>Jenis Kelamin</td>
+                        <td>Tempat, Tanggal Lahir</td>
+                        <td>Kelas</td>
+                        <td>Lulusan</td>
+                        <td>Status Lulusan</td>
+                        <td>Status Lanjutan</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td class="number"></td>
+                        <td>Muhamad Ramdani Hidayatullah</td>
+                        <td>9961929832</td>
+                        <td>Laki-laki</td>
+                        <td>Karawang, 30 Januari 1997</td>
+                        <td>12 IPA 1</td>
+                        <td>2015</td>
+                        <td>Kuliah</td>
+                        <td>STMIK Rosma</td>
+                    </tr>
+                    <tr>
+                        <td class="number"></td>
+                        <td>Adhinda Putri Larasati</td>
+                        <td>9961929832</td>
+                        <td>Perempuan</td>
+                        <td>Jakarta, 19 Juli 2001</td>
+                        <td>12 IPA 3</td>
+                        <td>2019</td>
+                        <td>Kerja</td>
+                        <td>Alfamart</td>
+                    </tr>
+                    <tr>
+                        <td class="number"></td>
+                        <td>Tri Indah Ayu Widari</td>
+                        <td>9961929863</td>
+                        <td>Perempuan</td>
+                        <td>Brebes, 21 Maret 2001</td>
+                        <td>12 IPA 2</td>
+                        <td>2019</td>
+                        <td>Kerja</td>
+                        <td>JVC Electronics</td>
+                    </tr>
+                    <tr>
+                        <td class="number"></td>
+                        <td>Erna Herlina</td>
+                        <td>0020938387</td>
+                        <td>Perempuan</td>
+                        <td>Karawang, 18 September 2001</td>
+                        <td>12 TKJ 1</td>
+                        <td>2020</td>
+                        <td>Kuliah</td>
+                        <td>UIN Sunan Gunung Djati</td>
+                    </tr>
+                    <tr>
+                        <td class="number"></td>
+                        <td>Kemala Putri</td>
+                        <td>0042884271</td>
+                        <td>Perempuan</td>
+                        <td>Karawang, 20 Agustus 2004</td>
+                        <td>12 RPL 1</td>
+                        <td>2021</td>
+                        <td>Kuliah</td>
+                        <td>Universitas Pasundan</td>
+                    </tr>
+                    <tr>
+                        <td class="number"></td>
+                        <td>Muhamad Ramdani Hidayatullah</td>
+                        <td>9961929832</td>
+                        <td>Laki-laki</td>
+                        <td>Karawang, 30 Januari 1997</td>
+                        <td>12 IPA 1</td>
+                        <td>2015</td>
+                        <td>Kuliah</td>
+                        <td>STMIK Rosma</td>
+                    </tr>
+                    <tr>
+                        <td class="number"></td>
+                        <td>Adhinda Putri Larasati</td>
+                        <td>9961929832</td>
+                        <td>Perempuan</td>
+                        <td>Jakarta, 19 Juli 2001</td>
+                        <td>12 IPA 3</td>
+                        <td>2019</td>
+                        <td>Kerja</td>
+                        <td>Alfamart</td>
+                    </tr>
+                    <tr>
+                        <td class="number"></td>
+                        <td>Tri Indah Ayu Widari</td>
+                        <td>9961929863</td>
+                        <td>Perempuan</td>
+                        <td>Brebes, 21 Maret 2001</td>
+                        <td>12 IPA 2</td>
+                        <td>2019</td>
+                        <td>Kerja</td>
+                        <td>JVC Electronics</td>
+                    </tr>
+                    <tr>
+                        <td class="number"></td>
+                        <td>Erna Herlina</td>
+                        <td>0020938387</td>
+                        <td>Perempuan</td>
+                        <td>Karawang, 18 September 2001</td>
+                        <td>12 TKJ 1</td>
+                        <td>2020</td>
+                        <td>Kuliah</td>
+                        <td>UIN Sunan Gunung Djati</td>
+                    </tr>
+                    <tr>
+                        <td class="number"></td>
+                        <td>Kemala Putri</td>
+                        <td>0042884271</td>
+                        <td>Perempuan</td>
+                        <td>Karawang, 20 Agustus 2004</td>
+                        <td>12 RPL 1</td>
+                        <td>2021</td>
+                        <td>Kuliah</td>
+                        <td>Universitas Pasundan</td>
+                    </tr>
+                    <tr>
+                        <td class="number"></td>
+                        <td>Muhamad Ramdani Hidayatullah</td>
+                        <td>9961929832</td>
+                        <td>Laki-laki</td>
+                        <td>Karawang, 30 Januari 1997</td>
+                        <td>12 IPA 1</td>
+                        <td>2015</td>
+                        <td>Kuliah</td>
+                        <td>STMIK Rosma</td>
+                    </tr>
+                    <tr>
+                        <td class="number"></td>
+                        <td>Adhinda Putri Larasati</td>
+                        <td>9961929832</td>
+                        <td>Perempuan</td>
+                        <td>Jakarta, 19 Juli 2001</td>
+                        <td>12 IPA 3</td>
+                        <td>2019</td>
+                        <td>Kerja</td>
+                        <td>Alfamart</td>
+                    </tr>
+                    <tr>
+                        <td class="number"></td>
+                        <td>Tri Indah Ayu Widari</td>
+                        <td>9961929863</td>
+                        <td>Perempuan</td>
+                        <td>Brebes, 21 Maret 2001</td>
+                        <td>12 IPA 2</td>
+                        <td>2019</td>
+                        <td>Kerja</td>
+                        <td>JVC Electronics</td>
+                    </tr>
+                    <tr>
+                        <td class="number"></td>
+                        <td>Erna Herlina</td>
+                        <td>0020938387</td>
+                        <td>Perempuan</td>
+                        <td>Karawang, 18 September 2001</td>
+                        <td>12 TKJ 1</td>
+                        <td>2020</td>
+                        <td>Kuliah</td>
+                        <td>UIN Sunan Gunung Djati</td>
+                    </tr>
+                    <tr>
+                        <td class="number"></td>
+                        <td>Kemala Putri</td>
+                        <td>0042884271</td>
+                        <td>Perempuan</td>
+                        <td>Karawang, 20 Agustus 2004</td>
+                        <td>12 RPL 1</td>
+                        <td>2021</td>
+                        <td>Kuliah</td>
+                        <td>Universitas Pasundan</td>
+                    </tr>
+                    <tr>
+                        <td class="number"></td>
+                        <td>Muhamad Ramdani Hidayatullah</td>
+                        <td>9961929832</td>
+                        <td>Laki-laki</td>
+                        <td>Karawang, 30 Januari 1997</td>
+                        <td>12 IPA 1</td>
+                        <td>2015</td>
+                        <td>Kuliah</td>
+                        <td>STMIK Rosma</td>
+                    </tr>
+                    <tr>
+                        <td class="number"></td>
+                        <td>Adhinda Putri Larasati</td>
+                        <td>9961929832</td>
+                        <td>Perempuan</td>
+                        <td>Jakarta, 19 Juli 2001</td>
+                        <td>12 IPA 3</td>
+                        <td>2019</td>
+                        <td>Kerja</td>
+                        <td>Alfamart</td>
+                    </tr>
+                    <tr>
+                        <td class="number"></td>
+                        <td>Tri Indah Ayu Widari</td>
+                        <td>9961929863</td>
+                        <td>Perempuan</td>
+                        <td>Brebes, 21 Maret 2001</td>
+                        <td>12 IPA 2</td>
+                        <td>2019</td>
+                        <td>Kerja</td>
+                        <td>JVC Electronics</td>
+                    </tr>
+                    <tr>
+                        <td class="number"></td>
+                        <td>Erna Herlina</td>
+                        <td>0020938387</td>
+                        <td>Perempuan</td>
+                        <td>Karawang, 18 September 2001</td>
+                        <td>12 TKJ 1</td>
+                        <td>2020</td>
+                        <td>Kuliah</td>
+                        <td>UIN Sunan Gunung Djati</td>
+                    </tr>
+                    <tr>
+                        <td class="number"></td>
+                        <td>Kemala Putri</td>
+                        <td>0042884271</td>
+                        <td>Perempuan</td>
+                        <td>Karawang, 20 Agustus 2004</td>
+                        <td>12 RPL 1</td>
+                        <td>2021</td>
+                        <td>Kuliah</td>
+                        <td>Universitas Pasundan</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+@endsection

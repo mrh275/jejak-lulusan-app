b9cef6c98ff8aabddb3ad0237bc91e89f8b0e0c4
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2022-01-28 18:33:53 +0700

    added datatable libiraries

--- resources/views/layout/admin/main.blade.php
+++ resources/views/layout/admin/main.blade.php
@@ -11,21 +11,26 @@
 
     <!-- CSS -->
     <link rel="stylesheet" href="{{ url('assets/admin/css') }}/admin.css">
-    <link rel="stylesheet" href="{{ url('assets/admin/css') }}/page.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href='https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css' rel='stylesheet'>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css" />
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.11.3/css/jquery.dataTables.min.css">
+    <link rel="stylesheet" href="{{ url('assets/admin/css') }}/page.css">
 
     <!-- Javascript -->
     <script src="{{ url('assets/admin/js') }}/jquery.min.js"></script>
-    <script src="https://code.jquery.com/ui/1.13.0/jquery-ui.js"></script>
+    <script src="https://code.jquery.com/ui/1.13.0/jquery-ui.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+    <script src="https://cdn.datatables.net/1.11.3/js/jquery.dataTables.min.js"></script>
 
-    @if ($activeSubMenu == 'biodata' || 'data-orangtua')
+    @if (request()->segment(2) === 'biodata' || 'data-orangtua')
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/vanillajs-datepicker@1.2.0/dist/css/datepicker.min.css">
         <script src="https://cdn.jsdelivr.net/npm/vanillajs-datepicker@1.2.0/dist/js/datepicker.min.js"></script>
+    @else
+        {{ '' }}
     @endif
+
 
 </head>
 
@@ -73,6 +78,32 @@
                 autohide: true,
             });
         })
+
+        // List Post Datatable
+        $(document).ready(function() {
+            $('.table-biodata').DataTable({
+                "lengthMenu": [
+                    [10, 15, 25, 50, 100, -1],
+                    [10, 15, 25, 50, 100, "All"]
+                ],
+                "pagingType": "full_numbers",
+                "language": {
+                    "paginate": {
+                        "previous": "<i class='bx bx-chevron-left'></i>",
+                        "next": "<i class='bx bx-chevron-right' ></i>"
+                    }
+                },
+            });
+        });
+
+        // Selecting table row
+        $('.table-biodata').on('click', 'tr', function() {
+            $('tr').not(this).removeClass('selected');
+            $(this).toggleClass('selected')
+        });
+        $('.table-biodata').on('click', 'button', function(e) {
+            e.preventDefault();
+        });
     </script>
 </body>
 

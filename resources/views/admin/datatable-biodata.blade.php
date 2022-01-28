2011571e32cc61d484a793862a3eb7cd0fe3e9f8
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2022-01-28 18:33:08 +0700

    import view datatable biodata from jejak-lulusan-admin

---
+++ resources/views/admin/datatable-biodata.blade.php
@@ -0,0 +1,248 @@
+@extends('layout.admin.main')
+
+@section('content')
+    <div class="content">
+        <h1 class="content-title">Tabel Biodata Lulusan</h1>
+
+        <div class="container">
+            <table class="table-biodata stripe hover">
+                <thead>
+                    <tr>
+                        <th>No</th>
+                        <th>Nama</th>
+                        <th>NISN</th>
+                        <th>NIS</th>
+                        <th>TTL</th>
+                        <th>Kelas</th>
+                        <th>Tahun Lulus</th>
+                        <th>Status Lulusan</th>
+                        <th>Nama Univ/PT</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td class="number"></td>
+                        <td class="nama">Muhamad Ramdani Hidayatullah</td>
+                        <td>9987452632</td>
+                        <td>121310432</td>
+                        <td>Karawang, 30 Januari 1997</td>
+                        <td>12 IPA 1</td>
+                        <td>2015</td>
+                        <td>Kuliah - Kerja</td>
+                        <td>STMIK Rosma - SMAN 1 Rawamerta</td>
+                    </tr>
+                    <tr>
+                        <td class="number"></td>
+                        <td class="nama">Erna Herlina</td>
+                        <td>0023489398</td>
+                        <td>181910219</td>
+                        <td>Karawang, 08 September 2002</td>
+                        <td>12 IPA 2</td>
+                        <td>2020</td>
+                        <td>Kuliah</td>
+                        <td>Universitas Buana Perjuangan</td>
+                    </tr>
+                    <tr>
+                        <td class="number"></td>
+                        <td class="nama">Adhinda Putri Larasati</td>
+                        <td>0018748391</td>
+                        <td>171810049</td>
+                        <td>Jakarta, 29 Juni 2001</td>
+                        <td>12 IPA 3</td>
+                        <td>2019</td>
+                        <td>Kerja</td>
+                        <td>Alfamart</td>
+                    </tr>
+                    <tr>
+                        <td class="number"></td>
+                        <td class="nama">Tri Indah Ayu Widari</td>
+                        <td>0013689357</td>
+                        <td>171810093</td>
+                        <td>Brebes, 08 Agustus 2001</td>
+                        <td>12 TKJ 2</td>
+                        <td>2019</td>
+                        <td>Kuliah - Kerja</td>
+                        <td>STMIK Rosma - JVC Electronics</td>
+                    </tr>
+                    <tr>
+                        <td class="number"></td>
+                        <td class="nama">Eman</td>
+                        <td>9959213748</td>
+                        <td>111210066</td>
+                        <td>Karawang, 06 Mei 1995</td>
+                        <td>12 IPS 3</td>
+                        <td>2012</td>
+                        <td>Kuliah - Kerja</td>
+                        <td>Unsika - Perum Peruri</td>
+                    </tr>
+                    <tr>
+                        <td class="number"></td>
+                        <td class="nama">Muhamad Ramdani Hidayatullah</td>
+                        <td>9987452632</td>
+                        <td>121310432</td>
+                        <td>Karawang, 30 Januari 1997</td>
+                        <td>12 IPA 1</td>
+                        <td>2015</td>
+                        <td>Kuliah - Kerja</td>
+                        <td>STMIK Rosma - SMAN 1 Rawamerta</td>
+                    </tr>
+                    <tr>
+                        <td class="number"></td>
+                        <td class="nama">Erna Herlina</td>
+                        <td>0023489398</td>
+                        <td>181910219</td>
+                        <td>Karawang, 08 September 2002</td>
+                        <td>12 IPA 2</td>
+                        <td>2020</td>
+                        <td>Kuliah</td>
+                        <td>Universitas Buana Perjuangan</td>
+                    </tr>
+                    <tr>
+                        <td class="number"></td>
+                        <td class="nama">Adhinda Putri Larasati</td>
+                        <td>0018748391</td>
+                        <td>171810049</td>
+                        <td>Jakarta, 29 Juni 2001</td>
+                        <td>12 IPA 3</td>
+                        <td>2019</td>
+                        <td>Kerja</td>
+                        <td>Alfamart</td>
+                    </tr>
+                    <tr>
+                        <td class="number"></td>
+                        <td class="nama">Tri Indah Ayu Widari</td>
+                        <td>0013689357</td>
+                        <td>171810093</td>
+                        <td>Brebes, 08 Agustus 2001</td>
+                        <td>12 TKJ 2</td>
+                        <td>2019</td>
+                        <td>Kuliah - Kerja</td>
+                        <td>STMIK Rosma - JVC Electronics</td>
+                    </tr>
+                    <tr>
+                        <td class="number"></td>
+                        <td class="nama">Eman</td>
+                        <td>9959213748</td>
+                        <td>111210066</td>
+                        <td>Karawang, 06 Mei 1995</td>
+                        <td>12 IPS 3</td>
+                        <td>2012</td>
+                        <td>Kuliah - Kerja</td>
+                        <td>Unsika - Perum Peruri</td>
+                    </tr>
+                    <tr>
+                        <td class="number"></td>
+                        <td class="nama">Muhamad Ramdani Hidayatullah</td>
+                        <td>9987452632</td>
+                        <td>121310432</td>
+                        <td>Karawang, 30 Januari 1997</td>
+                        <td>12 IPA 1</td>
+                        <td>2015</td>
+                        <td>Kuliah - Kerja</td>
+                        <td>STMIK Rosma - SMAN 1 Rawamerta</td>
+                    </tr>
+                    <tr>
+                        <td class="number"></td>
+                        <td class="nama">Erna Herlina</td>
+                        <td>0023489398</td>
+                        <td>181910219</td>
+                        <td>Karawang, 08 September 2002</td>
+                        <td>12 IPA 2</td>
+                        <td>2020</td>
+                        <td>Kuliah</td>
+                        <td>Universitas Buana Perjuangan</td>
+                    </tr>
+                    <tr>
+                        <td class="number"></td>
+                        <td class="nama">Adhinda Putri Larasati</td>
+                        <td>0018748391</td>
+                        <td>171810049</td>
+                        <td>Jakarta, 29 Juni 2001</td>
+                        <td>12 IPA 3</td>
+                        <td>2019</td>
+                        <td>Kerja</td>
+                        <td>Alfamart</td>
+                    </tr>
+                    <tr>
+                        <td class="number"></td>
+                        <td class="nama">Tri Indah Ayu Widari</td>
+                        <td>0013689357</td>
+                        <td>171810093</td>
+                        <td>Brebes, 08 Agustus 2001</td>
+                        <td>12 TKJ 2</td>
+                        <td>2019</td>
+                        <td>Kuliah - Kerja</td>
+                        <td>STMIK Rosma - JVC Electronics</td>
+                    </tr>
+                    <tr>
+                        <td class="number"></td>
+                        <td class="nama">Eman</td>
+                        <td>9959213748</td>
+                        <td>111210066</td>
+                        <td>Karawang, 06 Mei 1995</td>
+                        <td>12 IPS 3</td>
+                        <td>2012</td>
+                        <td>Kuliah - Kerja</td>
+                        <td>Unsika - Perum Peruri</td>
+                    </tr>
+                    <tr>
+                        <td class="number"></td>
+                        <td class="nama">Muhamad Ramdani Hidayatullah</td>
+                        <td>9987452632</td>
+                        <td>121310432</td>
+                        <td>Karawang, 30 Januari 1997</td>
+                        <td>12 IPA 1</td>
+                        <td>2015</td>
+                        <td>Kuliah - Kerja</td>
+                        <td>STMIK Rosma - SMAN 1 Rawamerta</td>
+                    </tr>
+                    <tr>
+                        <td class="number"></td>
+                        <td class="nama">Erna Herlina</td>
+                        <td>0023489398</td>
+                        <td>181910219</td>
+                        <td>Karawang, 08 September 2002</td>
+                        <td>12 IPA 2</td>
+                        <td>2020</td>
+                        <td>Kuliah</td>
+                        <td>Universitas Buana Perjuangan</td>
+                    </tr>
+                    <tr>
+                        <td class="number"></td>
+                        <td class="nama">Adhinda Putri Larasati</td>
+                        <td>0018748391</td>
+                        <td>171810049</td>
+                        <td>Jakarta, 29 Juni 2001</td>
+                        <td>12 IPA 3</td>
+                        <td>2019</td>
+                        <td>Kerja</td>
+                        <td>Alfamart</td>
+                    </tr>
+                    <tr>
+                        <td class="number"></td>
+                        <td class="nama">Tri Indah Ayu Widari</td>
+                        <td>0013689357</td>
+                        <td>171810093</td>
+                        <td>Brebes, 08 Agustus 2001</td>
+                        <td>12 TKJ 2</td>
+                        <td>2019</td>
+                        <td>Kuliah - Kerja</td>
+                        <td>STMIK Rosma - JVC Electronics</td>
+                    </tr>
+                    <tr>
+                        <td class="number"></td>
+                        <td class="nama">Eman</td>
+                        <td>9959213748</td>
+                        <td>111210066</td>
+                        <td>Karawang, 06 Mei 1995</td>
+                        <td>12 IPS 3</td>
+                        <td>2012</td>
+                        <td>Kuliah - Kerja</td>
+                        <td>Unsika - Perum Peruri</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+
+    </div>
+@endsection

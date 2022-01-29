5bce3251ccbcca9edaa6eb7b19934f0955601ba5
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2022-01-29 11:16:04 +0700

    import datatable kuliah from jejak-lulusan-admin

---
+++ resources/views/admin/datatable-kuliah.blade.php
@@ -0,0 +1,248 @@
+@extends('layout.admin.main')
+
+@section('content')
+    <div class="content">
+        <h1 class="content-title">Tabel Data Kuliah Lulusan</h1>
+
+        <div class="container">
+            <table class="table-biodata stripe hover">
+                <thead>
+                    <tr>
+                        <th>No</th>
+                        <th>Nama</th>
+                        <th>NISN</th>
+                        <th>TTL</th>
+                        <th>Kelas</th>
+                        <th>Tahun Lulus</th>
+                        <th>Nama Universitas</th>
+                        <th>Fakultas</th>
+                        <th>Jurusan/Prodi</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td class="number"></td>
+                        <td class="nama">Muhamad Ramdani Hidayatullah</td>
+                        <td>9987452632</td>
+                        <td>Karawang, 30 Januari 1997</td>
+                        <td>12 IPA 1</td>
+                        <td>2015</td>
+                        <td>STMIK Rosma</td>
+                        <td>Teknik</td>
+                        <td>Teknik Informatika</td>
+                    </tr>
+                    <tr>
+                        <td class="number"></td>
+                        <td class="nama">Erna Herlina</td>
+                        <td>0023489398</td>
+                        <td>Karawang, 08 September 2002</td>
+                        <td>12 IPA 2</td>
+                        <td>2020</td>
+                        <td>Universitas Buana Perjuangan</td>
+                        <td>Ekonomi dan Bisnis</td>
+                        <td>Ekonomi Syariah</td>
+                    </tr>
+                    <tr>
+                        <td class="number"></td>
+                        <td class="nama">Adhinda Putri Larasati</td>
+                        <td>0018748391</td>
+                        <td>Jakarta, 29 Juni 2001</td>
+                        <td>12 IPA 3</td>
+                        <td>2019</td>
+                        <td>Universitas Singaperbangsa Karawang</td>
+                        <td>MIPA</td>
+                        <td>Pendidikan Matematika</td>
+                    </tr>
+                    <tr>
+                        <td class="number"></td>
+                        <td class="nama">Tri Indah Ayu Widari</td>
+                        <td>0013689357</td>
+                        <td>Brebes, 08 Agustus 2001</td>
+                        <td>12 TKJ 2</td>
+                        <td>2019</td>
+                        <td>UIN Sunan Gunung Djati</td>
+                        <td>Tarbiyah</td>
+                        <td>Pendidikan Agama Islam</td>
+                    </tr>
+                    <tr>
+                        <td class="number"></td>
+                        <td class="nama">Eman</td>
+                        <td>9959213748</td>
+                        <td>Karawang, 06 Mei 1995</td>
+                        <td>12 IPS 3</td>
+                        <td>2012</td>
+                        <td>Universitas Pasundan</td>
+                        <td>Teknik</td>
+                        <td>Teknik Industri</td>
+                    </tr>
+                    <tr>
+                        <td class="number"></td>
+                        <td class="nama">Muhamad Ramdani Hidayatullah</td>
+                        <td>9987452632</td>
+                        <td>Karawang, 30 Januari 1997</td>
+                        <td>12 IPA 1</td>
+                        <td>2015</td>
+                        <td>STMIK Rosma</td>
+                        <td>Teknik</td>
+                        <td>Teknik Informatika</td>
+                    </tr>
+                    <tr>
+                        <td class="number"></td>
+                        <td class="nama">Erna Herlina</td>
+                        <td>0023489398</td>
+                        <td>Karawang, 08 September 2002</td>
+                        <td>12 IPA 2</td>
+                        <td>2020</td>
+                        <td>Universitas Buana Perjuangan</td>
+                        <td>Ekonomi dan Bisnis</td>
+                        <td>Ekonomi Syariah</td>
+                    </tr>
+                    <tr>
+                        <td class="number"></td>
+                        <td class="nama">Adhinda Putri Larasati</td>
+                        <td>0018748391</td>
+                        <td>Jakarta, 29 Juni 2001</td>
+                        <td>12 IPA 3</td>
+                        <td>2019</td>
+                        <td>Universitas Singaperbangsa Karawang</td>
+                        <td>MIPA</td>
+                        <td>Pendidikan Matematika</td>
+                    </tr>
+                    <tr>
+                        <td class="number"></td>
+                        <td class="nama">Tri Indah Ayu Widari</td>
+                        <td>0013689357</td>
+                        <td>Brebes, 08 Agustus 2001</td>
+                        <td>12 TKJ 2</td>
+                        <td>2019</td>
+                        <td>UIN Sunan Gunung Djati</td>
+                        <td>Tarbiyah</td>
+                        <td>Pendidikan Agama Islam</td>
+                    </tr>
+                    <tr>
+                        <td class="number"></td>
+                        <td class="nama">Eman</td>
+                        <td>9959213748</td>
+                        <td>Karawang, 06 Mei 1995</td>
+                        <td>12 IPS 3</td>
+                        <td>2012</td>
+                        <td>Universitas Pasundan</td>
+                        <td>Teknik</td>
+                        <td>Teknik Industri</td>
+                    </tr>
+                    <tr>
+                        <td class="number"></td>
+                        <td class="nama">Muhamad Ramdani Hidayatullah</td>
+                        <td>9987452632</td>
+                        <td>Karawang, 30 Januari 1997</td>
+                        <td>12 IPA 1</td>
+                        <td>2015</td>
+                        <td>STMIK Rosma</td>
+                        <td>Teknik</td>
+                        <td>Teknik Informatika</td>
+                    </tr>
+                    <tr>
+                        <td class="number"></td>
+                        <td class="nama">Erna Herlina</td>
+                        <td>0023489398</td>
+                        <td>Karawang, 08 September 2002</td>
+                        <td>12 IPA 2</td>
+                        <td>2020</td>
+                        <td>Universitas Buana Perjuangan</td>
+                        <td>Ekonomi dan Bisnis</td>
+                        <td>Ekonomi Syariah</td>
+                    </tr>
+                    <tr>
+                        <td class="number"></td>
+                        <td class="nama">Adhinda Putri Larasati</td>
+                        <td>0018748391</td>
+                        <td>Jakarta, 29 Juni 2001</td>
+                        <td>12 IPA 3</td>
+                        <td>2019</td>
+                        <td>Universitas Singaperbangsa Karawang</td>
+                        <td>MIPA</td>
+                        <td>Pendidikan Matematika</td>
+                    </tr>
+                    <tr>
+                        <td class="number"></td>
+                        <td class="nama">Tri Indah Ayu Widari</td>
+                        <td>0013689357</td>
+                        <td>Brebes, 08 Agustus 2001</td>
+                        <td>12 TKJ 2</td>
+                        <td>2019</td>
+                        <td>UIN Sunan Gunung Djati</td>
+                        <td>Tarbiyah</td>
+                        <td>Pendidikan Agama Islam</td>
+                    </tr>
+                    <tr>
+                        <td class="number"></td>
+                        <td class="nama">Eman</td>
+                        <td>9959213748</td>
+                        <td>Karawang, 06 Mei 1995</td>
+                        <td>12 IPS 3</td>
+                        <td>2012</td>
+                        <td>Universitas Pasundan</td>
+                        <td>Teknik</td>
+                        <td>Teknik Industri</td>
+                    </tr>
+                    <tr>
+                        <td class="number"></td>
+                        <td class="nama">Muhamad Ramdani Hidayatullah</td>
+                        <td>9987452632</td>
+                        <td>Karawang, 30 Januari 1997</td>
+                        <td>12 IPA 1</td>
+                        <td>2015</td>
+                        <td>STMIK Rosma</td>
+                        <td>Teknik</td>
+                        <td>Teknik Informatika</td>
+                    </tr>
+                    <tr>
+                        <td class="number"></td>
+                        <td class="nama">Erna Herlina</td>
+                        <td>0023489398</td>
+                        <td>Karawang, 08 September 2002</td>
+                        <td>12 IPA 2</td>
+                        <td>2020</td>
+                        <td>Universitas Buana Perjuangan</td>
+                        <td>Ekonomi dan Bisnis</td>
+                        <td>Ekonomi Syariah</td>
+                    </tr>
+                    <tr>
+                        <td class="number"></td>
+                        <td class="nama">Adhinda Putri Larasati</td>
+                        <td>0018748391</td>
+                        <td>Jakarta, 29 Juni 2001</td>
+                        <td>12 IPA 3</td>
+                        <td>2019</td>
+                        <td>Universitas Singaperbangsa Karawang</td>
+                        <td>MIPA</td>
+                        <td>Pendidikan Matematika</td>
+                    </tr>
+                    <tr>
+                        <td class="number"></td>
+                        <td class="nama">Tri Indah Ayu Widari</td>
+                        <td>0013689357</td>
+                        <td>Brebes, 08 Agustus 2001</td>
+                        <td>12 TKJ 2</td>
+                        <td>2019</td>
+                        <td>UIN Sunan Gunung Djati</td>
+                        <td>Tarbiyah</td>
+                        <td>Pendidikan Agama Islam</td>
+                    </tr>
+                    <tr>
+                        <td class="number"></td>
+                        <td class="nama">Eman</td>
+                        <td>9959213748</td>
+                        <td>Karawang, 06 Mei 1995</td>
+                        <td>12 IPS 3</td>
+                        <td>2012</td>
+                        <td>Universitas Pasundan</td>
+                        <td>Teknik</td>
+                        <td>Teknik Industri</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+
+    </div>
+@endsection

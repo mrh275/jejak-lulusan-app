8420bccaa848160844a94df0ed75bed7d1afc7d3
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2022-01-29 11:16:17 +0700

    import datatable pekerjaan from jejak-lulusan-admin

---
+++ resources/views/admin/datatable-pekerjaan.blade.php
@@ -0,0 +1,248 @@
+@extends('layout.admin.main')
+
+@section('content')
+    <div class="content">
+        <h1 class="content-title">Tabel Data Pekerjaan Lulusan</h1>
+
+        <div class="container">
+            <table class="table-biodata stripe hover">
+                <thead>
+                    <tr>
+                        <th>No</th>
+                        <th>Nama</th>
+                        <th>NISN</th>
+                        <th>TTL</th>
+                        <th>Kelas</th>
+                        <th>Tahun Lulus</th>
+                        <th>Nama Perusahaan</th>
+                        <th>Divisi/Bagian</th>
+                        <th>Jabatan</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td class="number"></td>
+                        <td class="nama">Muhamad Ramdani Hidayatullah</td>
+                        <td>9987452632</td>
+                        <td>Karawang, 30 Januari 1997</td>
+                        <td>12 IPA 1</td>
+                        <td>2015</td>
+                        <td>PT. Multiindo Mandiri</td>
+                        <td>Warehouse</td>
+                        <td>Administrator</td>
+                    </tr>
+                    <tr>
+                        <td class="number"></td>
+                        <td class="nama">Erna Herlina</td>
+                        <td>0023489398</td>
+                        <td>Karawang, 08 September 2002</td>
+                        <td>12 IPA 2</td>
+                        <td>2020</td>
+                        <td>Yamaha Motor Mfg. Indonesia</td>
+                        <td>Produksi</td>
+                        <td>Operator</td>
+                    </tr>
+                    <tr>
+                        <td class="number"></td>
+                        <td class="nama">Adhinda Putri Larasati</td>
+                        <td>0018748391</td>
+                        <td>Jakarta, 29 Juni 2001</td>
+                        <td>12 IPA 3</td>
+                        <td>2019</td>
+                        <td>Alfamart</td>
+                        <td>Retail</td>
+                        <td>Cashier</td>
+                    </tr>
+                    <tr>
+                        <td class="number"></td>
+                        <td class="nama">Tri Indah Ayu Widari</td>
+                        <td>0013689357</td>
+                        <td>Brebes, 08 Agustus 2001</td>
+                        <td>12 TKJ 2</td>
+                        <td>2019</td>
+                        <td>JVC Electronics</td>
+                        <td>QC</td>
+                        <td>Operator</td>
+                    </tr>
+                    <tr>
+                        <td class="number"></td>
+                        <td class="nama">Eman</td>
+                        <td>9959213748</td>
+                        <td>Karawang, 06 Mei 1995</td>
+                        <td>12 IPS 3</td>
+                        <td>2012</td>
+                        <td>Perum Peruri</td>
+                        <td>Produksi</td>
+                        <td>Operator</td>
+                    </tr>
+                    <tr>
+                        <td class="number"></td>
+                        <td class="nama">Muhamad Ramdani Hidayatullah</td>
+                        <td>9987452632</td>
+                        <td>Karawang, 30 Januari 1997</td>
+                        <td>12 IPA 1</td>
+                        <td>2015</td>
+                        <td>PT. Multiindo Mandiri</td>
+                        <td>Warehouse</td>
+                        <td>Administrator</td>
+                    </tr>
+                    <tr>
+                        <td class="number"></td>
+                        <td class="nama">Erna Herlina</td>
+                        <td>0023489398</td>
+                        <td>Karawang, 08 September 2002</td>
+                        <td>12 IPA 2</td>
+                        <td>2020</td>
+                        <td>Yamaha Motor Mfg. Indonesia</td>
+                        <td>Produksi</td>
+                        <td>Operator</td>
+                    </tr>
+                    <tr>
+                        <td class="number"></td>
+                        <td class="nama">Adhinda Putri Larasati</td>
+                        <td>0018748391</td>
+                        <td>Jakarta, 29 Juni 2001</td>
+                        <td>12 IPA 3</td>
+                        <td>2019</td>
+                        <td>Alfamart</td>
+                        <td>Retail</td>
+                        <td>Cashier</td>
+                    </tr>
+                    <tr>
+                        <td class="number"></td>
+                        <td class="nama">Tri Indah Ayu Widari</td>
+                        <td>0013689357</td>
+                        <td>Brebes, 08 Agustus 2001</td>
+                        <td>12 TKJ 2</td>
+                        <td>2019</td>
+                        <td>JVC Electronics</td>
+                        <td>QC</td>
+                        <td>Operator</td>
+                    </tr>
+                    <tr>
+                        <td class="number"></td>
+                        <td class="nama">Eman</td>
+                        <td>9959213748</td>
+                        <td>Karawang, 06 Mei 1995</td>
+                        <td>12 IPS 3</td>
+                        <td>2012</td>
+                        <td>Perum Peruri</td>
+                        <td>Produksi</td>
+                        <td>Operator</td>
+                    </tr>
+                    <tr>
+                        <td class="number"></td>
+                        <td class="nama">Muhamad Ramdani Hidayatullah</td>
+                        <td>9987452632</td>
+                        <td>Karawang, 30 Januari 1997</td>
+                        <td>12 IPA 1</td>
+                        <td>2015</td>
+                        <td>PT. Multiindo Mandiri</td>
+                        <td>Warehouse</td>
+                        <td>Administrator</td>
+                    </tr>
+                    <tr>
+                        <td class="number"></td>
+                        <td class="nama">Erna Herlina</td>
+                        <td>0023489398</td>
+                        <td>Karawang, 08 September 2002</td>
+                        <td>12 IPA 2</td>
+                        <td>2020</td>
+                        <td>Yamaha Motor Mfg. Indonesia</td>
+                        <td>Produksi</td>
+                        <td>Operator</td>
+                    </tr>
+                    <tr>
+                        <td class="number"></td>
+                        <td class="nama">Adhinda Putri Larasati</td>
+                        <td>0018748391</td>
+                        <td>Jakarta, 29 Juni 2001</td>
+                        <td>12 IPA 3</td>
+                        <td>2019</td>
+                        <td>Alfamart</td>
+                        <td>Retail</td>
+                        <td>Cashier</td>
+                    </tr>
+                    <tr>
+                        <td class="number"></td>
+                        <td class="nama">Tri Indah Ayu Widari</td>
+                        <td>0013689357</td>
+                        <td>Brebes, 08 Agustus 2001</td>
+                        <td>12 TKJ 2</td>
+                        <td>2019</td>
+                        <td>JVC Electronics</td>
+                        <td>QC</td>
+                        <td>Operator</td>
+                    </tr>
+                    <tr>
+                        <td class="number"></td>
+                        <td class="nama">Eman</td>
+                        <td>9959213748</td>
+                        <td>Karawang, 06 Mei 1995</td>
+                        <td>12 IPS 3</td>
+                        <td>2012</td>
+                        <td>Perum Peruri</td>
+                        <td>Produksi</td>
+                        <td>Operator</td>
+                    </tr>
+                    <tr>
+                        <td class="number"></td>
+                        <td class="nama">Muhamad Ramdani Hidayatullah</td>
+                        <td>9987452632</td>
+                        <td>Karawang, 30 Januari 1997</td>
+                        <td>12 IPA 1</td>
+                        <td>2015</td>
+                        <td>PT. Multiindo Mandiri</td>
+                        <td>Warehouse</td>
+                        <td>Administrator</td>
+                    </tr>
+                    <tr>
+                        <td class="number"></td>
+                        <td class="nama">Erna Herlina</td>
+                        <td>0023489398</td>
+                        <td>Karawang, 08 September 2002</td>
+                        <td>12 IPA 2</td>
+                        <td>2020</td>
+                        <td>Yamaha Motor Mfg. Indonesia</td>
+                        <td>Produksi</td>
+                        <td>Operator</td>
+                    </tr>
+                    <tr>
+                        <td class="number"></td>
+                        <td class="nama">Adhinda Putri Larasati</td>
+                        <td>0018748391</td>
+                        <td>Jakarta, 29 Juni 2001</td>
+                        <td>12 IPA 3</td>
+                        <td>2019</td>
+                        <td>Alfamart</td>
+                        <td>Retail</td>
+                        <td>Cashier</td>
+                    </tr>
+                    <tr>
+                        <td class="number"></td>
+                        <td class="nama">Tri Indah Ayu Widari</td>
+                        <td>0013689357</td>
+                        <td>Brebes, 08 Agustus 2001</td>
+                        <td>12 TKJ 2</td>
+                        <td>2019</td>
+                        <td>JVC Electronics</td>
+                        <td>QC</td>
+                        <td>Operator</td>
+                    </tr>
+                    <tr>
+                        <td class="number"></td>
+                        <td class="nama">Eman</td>
+                        <td>9959213748</td>
+                        <td>Karawang, 06 Mei 1995</td>
+                        <td>12 IPS 3</td>
+                        <td>2012</td>
+                        <td>Perum Peruri</td>
+                        <td>Produksi</td>
+                        <td>Operator</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+
+    </div>
+@endsection

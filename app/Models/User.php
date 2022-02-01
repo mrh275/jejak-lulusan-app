1a2303f486af421adac92a6ee1661b79babeed8a
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2022-02-01 23:48:42 +0700

    switch between fillable and guarded column

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,10 +17,14 @@ class User extends Authenticatable
      *
      * @var array<int, string>
      */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
+    // protected $fillable = [
+    //     'name',
+    //     'email',
+    //     'password',
+    // ];
+
+    protected $guarded = [
+        'id'
     ];
 
     /**

dcfbcac7714336290a0bb505c689d29420b15fe8
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2022-02-01 23:49:27 +0700

    Seed two user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +16,20 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        User::create([
+            'username'  => 'administrator',
+            'password'  => Hash::make('smanesta'),
+            'name'      => 'Administrator',
+            'email'     => 'admin@admin.com',
+            'is_admin'  => true,
+        ]);
+
+        User::create([
+            'username'  => 'operator',
+            'password'  => Hash::make('operator'),
+            'name'      => 'Operator',
+            'email'     => 'operator@operator.com',
+            'is_admin'  => false,
+        ]);
     }
 }

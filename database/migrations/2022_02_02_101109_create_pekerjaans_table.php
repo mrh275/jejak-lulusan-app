acf6e820b06f06c19a09da48843bf849470d1937
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2022-02-02 18:43:56 +0700

    created migration for create table pekerjaan

---
+++ database/migrations/2022_02_02_101109_create_pekerjaans_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePekerjaansTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pekerjaans', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('nisn_pekerjaan')->unique();
+            $table->string('nama_perusahaan');
+            $table->string('industri');
+            $table->string('divisi');
+            $table->string('jabatan');
+            $table->string('alamat_perusahaan');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pekerjaans');
+    }
+}

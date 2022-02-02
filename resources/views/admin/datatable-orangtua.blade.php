4c9b9b827446e84869c91e05000127aa09756ffd
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2022-02-02 21:35:29 +0700

    show data orang tua from database

--- resources/views/admin/datatable-orangtua.blade.php
+++ resources/views/admin/datatable-orangtua.blade.php
@@ -11,193 +11,24 @@
                         <th>No</th>
                         <th>Nama</th>
                         <th>NISN</th>
-                        <th>TTL</th>
                         <th>Kelas</th>
+                        <th>Tahun Lulus</th>
                         <th>Nama Ayah</th>
                         <th>Nama Ibu</th>
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td class="number"></td>
-                        <td class="nama text-left">Muhamad Ramdani Hidayatullah</td>
-                        <td>9987452632</td>
-                        <td>Karawang, 30 Januari 1997</td>
-                        <td>12 IPA 1</td>
-                        <td>Rudiyanto</td>
-                        <td>Siti Nursoleha</td>
-                    </tr>
-                    <tr>
-                        <td class="number"></td>
-                        <td class="nama text-left">Erna Herlina</td>
-                        <td>0023489398</td>
-                        <td>Karawang, 08 September 2002</td>
-                        <td>12 IPA 2</td>
-                        <td>Nana Nuryana</td>
-                        <td>Maesaroh</td>
-                    </tr>
-                    <tr>
-                        <td class="number"></td>
-                        <td class="nama text-left">Adhinda Putri Larasati</td>
-                        <td>0018748391</td>
-                        <td>Jakarta, 29 Juni 2001</td>
-                        <td>12 IPA 3</td>
-                        <td>Suparwoto</td>
-                        <td>Endah</td>
-                    </tr>
-                    <tr>
-                        <td class="number"></td>
-                        <td class="nama text-left">Tri Indah Ayu Widari</td>
-                        <td>0013689357</td>
-                        <td>Brebes, 08 Agustus 2001</td>
-                        <td>12 TKJ 2</td>
-                        <td>Bambang</td>
-                        <td>Sukiyem</td>
-                    </tr>
-                    <tr>
-                        <td class="number"></td>
-                        <td class="nama text-left">Eman</td>
-                        <td>9959213748</td>
-                        <td>Karawang, 06 Mei 1995</td>
-                        <td>12 IPS 3</td>
-                        <td>Suteja</td>
-                        <td>Maemunah</td>
-                    </tr>
-                    <tr>
-                        <td class="number"></td>
-                        <td class="nama text-left">Muhamad Ramdani Hidayatullah</td>
-                        <td>9987452632</td>
-                        <td>Karawang, 30 Januari 1997</td>
-                        <td>12 IPA 1</td>
-                        <td>Rudiyanto</td>
-                        <td>Siti Nursoleha</td>
-                    </tr>
-                    <tr>
-                        <td class="number"></td>
-                        <td class="nama text-left">Erna Herlina</td>
-                        <td>0023489398</td>
-                        <td>Karawang, 08 September 2002</td>
-                        <td>12 IPA 2</td>
-                        <td>Nana Nuryana</td>
-                        <td>Maesaroh</td>
-                    </tr>
-                    <tr>
-                        <td class="number"></td>
-                        <td class="nama text-left">Adhinda Putri Larasati</td>
-                        <td>0018748391</td>
-                        <td>Jakarta, 29 Juni 2001</td>
-                        <td>12 IPA 3</td>
-                        <td>Suparwoto</td>
-                        <td>Endah</td>
-                    </tr>
-                    <tr>
-                        <td class="number"></td>
-                        <td class="nama text-left">Tri Indah Ayu Widari</td>
-                        <td>0013689357</td>
-                        <td>Brebes, 08 Agustus 2001</td>
-                        <td>12 TKJ 2</td>
-                        <td>Bambang</td>
-                        <td>Sukiyem</td>
-                    </tr>
-                    <tr>
-                        <td class="number"></td>
-                        <td class="nama text-left">Eman</td>
-                        <td>9959213748</td>
-                        <td>Karawang, 06 Mei 1995</td>
-                        <td>12 IPS 3</td>
-                        <td>Suteja</td>
-                        <td>Maemunah</td>
-                    </tr>
-                    <tr>
-                        <td class="number"></td>
-                        <td class="nama text-left">Muhamad Ramdani Hidayatullah</td>
-                        <td>9987452632</td>
-                        <td>Karawang, 30 Januari 1997</td>
-                        <td>12 IPA 1</td>
-                        <td>Rudiyanto</td>
-                        <td>Siti Nursoleha</td>
-                    </tr>
-                    <tr>
-                        <td class="number"></td>
-                        <td class="nama text-left">Erna Herlina</td>
-                        <td>0023489398</td>
-                        <td>Karawang, 08 September 2002</td>
-                        <td>12 IPA 2</td>
-                        <td>Nana Nuryana</td>
-                        <td>Maesaroh</td>
-                    </tr>
-                    <tr>
-                        <td class="number"></td>
-                        <td class="nama text-left">Adhinda Putri Larasati</td>
-                        <td>0018748391</td>
-                        <td>Jakarta, 29 Juni 2001</td>
-                        <td>12 IPA 3</td>
-                        <td>Suparwoto</td>
-                        <td>Endah</td>
-                    </tr>
-                    <tr>
-                        <td class="number"></td>
-                        <td class="nama text-left">Tri Indah Ayu Widari</td>
-                        <td>0013689357</td>
-                        <td>Brebes, 08 Agustus 2001</td>
-                        <td>12 TKJ 2</td>
-                        <td>Bambang</td>
-                        <td>Sukiyem</td>
-                    </tr>
-                    <tr>
-                        <td class="number"></td>
-                        <td class="nama text-left">Eman</td>
-                        <td>9959213748</td>
-                        <td>Karawang, 06 Mei 1995</td>
-                        <td>12 IPS 3</td>
-                        <td>Suteja</td>
-                        <td>Maemunah</td>
-                    </tr>
-                    <tr>
-                        <td class="number"></td>
-                        <td class="nama text-left">Muhamad Ramdani Hidayatullah</td>
-                        <td>9987452632</td>
-                        <td>Karawang, 30 Januari 1997</td>
-                        <td>12 IPA 1</td>
-                        <td>Rudiyanto</td>
-                        <td>Siti Nursoleha</td>
-                    </tr>
-                    <tr>
-                        <td class="number"></td>
-                        <td class="nama text-left">Erna Herlina</td>
-                        <td>0023489398</td>
-                        <td>Karawang, 08 September 2002</td>
-                        <td>12 IPA 2</td>
-                        <td>Nana Nuryana</td>
-                        <td>Maesaroh</td>
-                    </tr>
-                    <tr>
-                        <td class="number"></td>
-                        <td class="nama text-left">Adhinda Putri Larasati</td>
-                        <td>0018748391</td>
-                        <td>Jakarta, 29 Juni 2001</td>
-                        <td>12 IPA 3</td>
-                        <td>Suparwoto</td>
-                        <td>Endah</td>
-                    </tr>
-                    <tr>
-                        <td class="number"></td>
-                        <td class="nama text-left">Tri Indah Ayu Widari</td>
-                        <td>0013689357</td>
-                        <td>Brebes, 08 Agustus 2001</td>
-                        <td>12 TKJ 2</td>
-                        <td>Bambang</td>
-                        <td>Sukiyem</td>
-                    </tr>
-                    <tr>
-                        <td class="number"></td>
-                        <td class="nama text-left">Eman</td>
-                        <td>9959213748</td>
-                        <td>Karawang, 06 Mei 1995</td>
-                        <td>12 IPS 3</td>
-                        <td>Suteja</td>
-                        <td>Maemunah</td>
-                    </tr>
+                    @foreach ($dataOrangTua as $data)
+                        <tr>
+                            <td>{{ $loop->iteration }}</td>
+                            <td class="nama text-left">{{ $data->nama }}</td>
+                            <td>{{ $data->nisn }}</td>
+                            <td>{{ $data->kelas }}</td>
+                            <td>{{ $data->tahun_lulus }}</td>
+                            <td>{{ $data->dataOrangTua->nama_ayah }}</td>
+                            <td>{{ $data->dataOrangTua->nama_ibu }}</td>
+                        </tr>
+                    @endforeach
                 </tbody>
             </table>
         </div>

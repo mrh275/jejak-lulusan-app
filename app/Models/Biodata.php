cbc86a5ac9de3ccf3f24d22f3c56e0f38034f4a8
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2022-02-02 18:40:44 +0700

    created biodata model

---
+++ app/Models/Biodata.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Kuliah;
+use App\Models\Pekerjaan;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Biodata extends Model
+{
+    use HasFactory;
+
+    protected $guarded = 'id';
+
+    public function kuliah()
+    {
+        return $this->hasOne(Kuliah::class, 'nisn_kuliah', 'nisn');
+    }
+
+    public function pekerjaan()
+    {
+        return $this->hasOne(Pekerjaan::class, 'nisn_pekerjaan', 'nisn');
+    }
+}

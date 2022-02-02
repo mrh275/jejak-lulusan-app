025d0258050cb43c51683395a40a31525ad5ca10
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2022-02-02 18:43:17 +0700

    created migration to create table kuliah

---
+++ database/migrations/2022_02_02_101011_create_biodatas_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBiodatasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('biodatas', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('nisn')->unique();
+            $table->bigInteger('nis')->unique();
+            $table->string('nama');
+            $table->string('jenis_kelamin');
+            $table->string('tempat_lahir');
+            $table->date('tanggal_lahir');
+            $table->string('kelas');
+            $table->integer('tahun_lulus');
+            $table->string('status_lulusan');
+            $table->string('alamat');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('biodatas');
+    }
+}

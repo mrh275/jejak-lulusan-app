62eb879a942a4a1122cb394a5920f4286f386add
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2022-02-02 21:33:59 +0700

    create migration to create data orang tua table on database

---
+++ database/migrations/2022_02_02_211345_create_data_orang_tuas_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDataOrangTuasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('data_orang_tuas', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('nisn_orang_tua')->unique();
+            $table->string('nama_ayah');
+            $table->string('jk_ayah');
+            $table->string('tempat_lahir_ayah');
+            $table->date('tanggal_lahir_ayah');
+            $table->string('pendidikan_ayah');
+            $table->string('pekerjaan_ayah');
+            $table->string('penghasilan_ayah');
+            $table->string('alamat_ayah');
+            $table->string('nama_ibu');
+            $table->string('jk_ibu');
+            $table->string('tempat_lahir_ibu');
+            $table->date('tanggal_lahir_ibu');
+            $table->string('pendidikan_ibu');
+            $table->string('pekerjaan_ibu');
+            $table->string('penghasilan_ibu');
+            $table->string('alamat_ibu');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('data_orang_tuas');
+    }
+}

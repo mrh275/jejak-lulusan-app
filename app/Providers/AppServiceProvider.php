7a7b9f644e2fe007adb9e7e7bb2d6a11d9e0b344
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2022-02-04 00:42:02 +0700

    created gate for admin and operator

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\User;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +25,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Gate::define('admin', function (User $user) {
+            return $user->is_admin;
+        });
+
+        Gate::define('operator', function (User $user) {
+            return !$user->is_admin;
+        });
     }
 }

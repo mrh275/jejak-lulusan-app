db1c8897fadc585ad55a467a28f8d326453ca660
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2022-02-05 17:49:26 +0700

    modified notifications panel

--- resources/views/component/nav-admin.blade.php
+++ resources/views/component/nav-admin.blade.php
@@ -6,29 +6,78 @@
         <div class="notification">
             <button class="btn-notif">
                 <i class='bx bxs-bell'></i>
-                <span class="badges" data-badge="*"></span>
+                @if (isset($notif['notifBio']) || isset($notif['notifOrangTua']) || isset($notif['notifKuliah']) || isset($notif['notifPekerjaan']))
+                    <span class="badges" data-badge="*"></span>
+                @endif
             </button>
             <ul class="nav-dropdown">
-                <li class="nav-drop-list">
-                    <a href="#" class="nav-drop-link">
-                        <i class='bx bx-comment-error'></i>
-                        <span class="nav-drop-name">New comments</span>
+                @if (isset($notif))
+                    @if (!isset($notif['notifBio']))
+                        <li class="nav-drop-list">
+                            <a href="/alumni/biodata" class="nav-drop-link">
+                                <i class='bx bxs-info-circle' style="color: #bf1f1f"></i>
+                                <span class="nav-drop-name">Biodata belum lengkap</span>
+                            </a>
+                        </li>
+                        <hr class="nav-drop-separator">
+                    @endif
+                    @if (!isset($notif['notifOrangTua']))
+                        <li class="nav-drop-list">
+                            <a href="/alumni/data-orangtua" class="nav-drop-link">
+                                <i class='bx bxs-info-circle' style="color: #bf1f1f"></i>
+                                <span class="nav-drop-name">Data Orang Tua belum lengkap</span>
+                            </a>
+                        </li>
+                        <hr class="nav-drop-separator">
+                    @endif
+                    @if ($biodata->status_lulusan == 1)
+                        @if (!isset($notif['notifKuliah']))
+                            <li class="nav-drop-list">
+                                <a href="/alumni/data-kuliah" class="nav-drop-link">
+                                    <i class='bx bxs-info-circle' style="color: #bf1f1f"></i>
+                                    <span class="nav-drop-name">Data Kuliah belum lengkap</span>
+                                </a>
+                            </li>
+                            <hr class="nav-drop-separator">
+                        @endif
+                    @endif
+                    @if ($biodata->status_lulusan == 2)
+                        @if (!isset($notif['notifPekerjaan']))
+                            <li class="nav-drop-list">
+                                <a href="/alumni/data-pekerjaan" class="nav-drop-link">
+                                    <i class='bx bxs-info-circle' style="color: #bf1f1f"></i>
+                                    <span class="nav-drop-name">Data Pekerjaan belum lengkap</span>
+                                </a>
+                            </li>
+                            <hr class="nav-drop-separator">
+                        @endif
+                    @endif
+                    @if ($biodata->status_lulusan == 3)
+                        @if (!isset($notif['notifKuliah']))
+                            <li class="nav-drop-list">
+                                <a href="/alumni/data-kuliah" class="nav-drop-link">
+                                    <i class='bx bxs-info-circle' style="color: #bf1f1f"></i>
+                                    <span class="nav-drop-name">Data Kuliah belum lengkap</span>
+                                </a>
+                            </li>
+                            <hr class="nav-drop-separator">
+                        @endif
+                        @if (!isset($notif['notifPekerjaan']))
+                            <li class="nav-drop-list">
+                                <a href="/alumni/data-pekerjaan" class="nav-drop-link">
+                                    <i class='bx bxs-info-circle' style="color: #bf1f1f"></i>
+                                    <span class="nav-drop-name">Data Pekerjaan belum lengkap</span>
+                                </a>
+                            </li>
+                            <hr class="nav-drop-separator">
+                        @endif
+                    @endif
+                @else
+                    <a href="javascript:void(0)" class="nav-drop-link">
+                        <i class='bx bx-info-circle'></i>
+                        <span class="nav-drop-name">Tidak ada notifikasi</span>
                     </a>
-                </li>
-                <hr class="nav-drop-separator">
-                <li class="nav-drop-list">
-                    <a href="#" class="nav-drop-link">
-                        <i class='bx bx-comment-error'></i>
-                        <span class="nav-drop-name">New comments</span>
-                    </a>
-                </li>
-                <hr class="nav-drop-separator">
-                <li class="nav-drop-list">
-                    <a href="#" class="nav-drop-link">
-                        <i class='bx bx-comment-error'></i>
-                        <span class="nav-drop-name">New comments</span>
-                    </a>
-                </li>
+                @endif
             </ul>
         </div>
         <div class="nav-user">

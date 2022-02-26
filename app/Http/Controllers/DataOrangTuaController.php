e866b135799954c7499a9b96aba72c029f200deb
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2022-02-26 11:31:20 +0700

    added insert and update data handler

--- app/Http/Controllers/DataOrangTuaController.php
+++ app/Http/Controllers/DataOrangTuaController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\DataOrangTua;
 use Illuminate\Http\Request;
 
 class DataOrangTuaController extends Controller
@@ -34,7 +35,47 @@ class DataOrangTuaController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $rules = [
+            'nama_ayah' => 'required',
+            'tempat_lahir_ayah' => 'required',
+            'pendidikan_ayah' => 'required',
+            'pekerjaan_ayah' => 'required',
+            'penghasilan_ayah' => 'required',
+            'alamat_ayah' => 'required',
+            'nama_ibu' => 'required',
+            'tempat_lahir_ibu' => 'required',
+            'pendidikan_ibu' => 'required',
+            'pekerjaan_ibu' => 'required',
+            'penghasilan_ibu' => 'required',
+            'alamat_ibu' => 'required',
+        ];
+
+        $errorMessage = [
+            'nama_ayah.required' => 'Nama Ayah tidak boleh kosong',
+            'tempat_lahir_ayah.required' => 'Tempat Lahir Ayah tidak boleh kosong',
+            'pendidikan_ayah.required' => 'Pendidikan Ayah tidak boleh kosong',
+            'pekerjaan_ayah.required' => 'Pekerjaan Ayah tidak boleh kosong',
+            'penghasilan_ayah.required' => 'Penghasilan Ayah tidak boleh kosong',
+            'alamat_ayah.required' => 'Alamat Ayah tidak boleh kosong',
+            'nama_ibu.required' => 'Nama Ibu tidak boleh kosong',
+            'tempat_lahir_ibu.required' => 'Tempat Lahir Ibu tidak boleh kosong',
+            'pendidikan_ibu.required' => 'Pendidikan Ibu tidak boleh kosong',
+            'pekerjaan_ibu.required' => 'Pekerjaan Ibu tidak boleh kosong',
+            'penghasilan_ibu.required' => 'Penghasilan Ibu tidak boleh kosong',
+            'alamat_ibu.required' => 'Alamat Ibu tidak boleh kosong',
+        ];
+
+        $validateData = $request->validate($rules, $errorMessage);
+        $tgl_lahir_ayah = str_replace('/', '-', $request->tanggal_lahir_ayah);
+        $tgl_lahir_ibu = str_replace('/', '-', $request->tanggal_lahir_ibu);
+        $validateData['tanggal_lahir_ayah'] = date('Y-m-d', strtotime($tgl_lahir_ayah));
+        $validateData['tanggal_lahir_ibu'] = date('Y-m-d', strtotime($tgl_lahir_ibu));
+        $validateData['nisn_orang_tua'] = $request->nisn_orang_tua;
+        $validateData['created_at'] = date('Y-m-d H:i:s');
+
+        DataOrangTua::insert($validateData);
+
+        return redirect('alumni/data-orangtua')->with('success', 'Data berhasil ditambahkan!');
     }
 
     /**
@@ -68,7 +109,45 @@ class DataOrangTuaController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $rules = [
+            'nama_ayah' => 'required',
+            'tempat_lahir_ayah' => 'required',
+            'pendidikan_ayah' => 'required',
+            'pekerjaan_ayah' => 'required',
+            'penghasilan_ayah' => 'required',
+            'alamat_ayah' => 'required',
+            'nama_ibu' => 'required',
+            'tempat_lahir_ibu' => 'required',
+            'pendidikan_ibu' => 'required',
+            'pekerjaan_ibu' => 'required',
+            'penghasilan_ibu' => 'required',
+            'alamat_ibu' => 'required',
+        ];
+
+        $errorMessage = [
+            'nama_ayah.required' => 'Nama Ayah tidak boleh kosong',
+            'tempat_lahir_ayah.required' => 'Tempat Lahir Ayah tidak boleh kosong',
+            'pendidikan_ayah.required' => 'Pendidikan Ayah tidak boleh kosong',
+            'pekerjaan_ayah.required' => 'Pekerjaan Ayah tidak boleh kosong',
+            'penghasilan_ayah.required' => 'Penghasilan Ayah tidak boleh kosong',
+            'alamat_ayah.required' => 'Alamat Ayah tidak boleh kosong',
+            'nama_ibu.required' => 'Nama Ibu tidak boleh kosong',
+            'tempat_lahir_ibu.required' => 'Tempat Lahir Ibu tidak boleh kosong',
+            'pendidikan_ibu.required' => 'Pendidikan Ibu tidak boleh kosong',
+            'pekerjaan_ibu.required' => 'Pekerjaan Ibu tidak boleh kosong',
+            'penghasilan_ibu.required' => 'Penghasilan Ibu tidak boleh kosong',
+            'alamat_ibu.required' => 'Alamat Ibu tidak boleh kosong',
+        ];
+
+        $validateData = $request->validate($rules, $errorMessage);
+        $tgl_lahir_ayah = str_replace('/', '-', $request->tanggal_lahir_ayah);
+        $tgl_lahir_ibu = str_replace('/', '-', $request->tanggal_lahir_ibu);
+        $validateBiodata['tanggal_lahir_ayah'] = date('Y-m-d', strtotime($tgl_lahir_ayah));
+        $validateBiodata['tanggal_lahir_ibu'] = date('Y-m-d', strtotime($tgl_lahir_ibu));
+
+        DataOrangTua::where('nisn_orang_tua', $id)->update($validateData);
+
+        return redirect('alumni/data-orangtua')->with('success', 'Data berhasil diubah!');
     }
 
     /**

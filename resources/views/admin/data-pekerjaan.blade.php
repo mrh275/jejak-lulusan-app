2b6dd8bebe2a3d40345521d6b38a1efc0c06746d
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2022-02-28 18:00:36 +0700

    added session checking

--- resources/views/admin/data-pekerjaan.blade.php
+++ resources/views/admin/data-pekerjaan.blade.php
@@ -4,9 +4,32 @@
     <div class="content">
         <h1 class="content-title">Form Data Pekerjaan Lulusan</h1>
 
+        @if (session()->has('success'))
+        <div class="alert alert-success">
+            {{ session()->get('success') }}
+        </div>
+        @endif
+        @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+
         <div class="container">
-            <form action="" class="form-group" method="post">
+            @if (isset($job->nisn_pekerjaan))
+                <form action="{{ url('pekerjaan/' . $job->nisn_pekerjaan) }}" class="form-group" method="post">
+                @method('put')
                 @csrf
+            @else
+                <form action="{{ url('pekerjaan') }}" class="form-group" method="post">
+                    @csrf
+                    <input type="hidden" name="nisn_pekerjaan" value="{{ session()->get('nisn') }}">
+            @endif
+
                 <div class="form-wrapper">
                     <div class="label-wrapper">
                         <label class="label-form" for="input-nama">Nama Lengkap</label>
@@ -56,7 +79,7 @@
                         <label class="label-form" for="input-perusahaan">Nama Perusahaan</label>
                     </div>
                     <div class="input-wrapper">
-                        <input name="perusahaan" class="form-control" id="input-perusahaan" value="{{ $job->nama_perusahaan ?? '' }}">
+                        <input type="text" name="nama_perusahaan" class="form-control" id="input-perusahaan" value="{{ old('nama_perusahaan', $job->nama_perusahaan ?? '') }}">
                     </div>
                 </div>
                 <div class="form-wrapper">
@@ -64,7 +87,7 @@
                         <label class="label-form" for="input-jenis-industri">Bidang Industri</label>
                     </div>
                     <div class="input-wrapper">
-                        <input name="jenis-industri" class="form-control" id="input-jenis-industri" value="{{ $job->industri ?? '' }}">
+                        <input type="text" name="industri" class="form-control" id="input-jenis-industri" value="{{ old('industri', $job->industri ?? '') }}">
                     </div>
                 </div>
                 <div class="form-wrapper">
@@ -72,29 +95,29 @@
                         <label class="label-form" for="input-divisi">Divisi/Bagian</label>
                     </div>
                     <div class="input-wrapper">
-                        <input name="divisi" class="form-control" id="input-divisi" value="{{ $job->divisi ?? '' }}"">
-                                        </div>
-                                    </div>
-                                    <div class="     form-wrapper">
-                        <div class="label-wrapper">
-                            <label class="label-form" for="input-jabatan">Jabatan</label>
-                        </div>
-                        <div class="input-wrapper">
-                            <input name="jabatan" class="form-control" id="input-jabatan" value="{{ $job->jabatan ?? '' }}"">
-                                        </div>
-                                    </div>
-                                    <div class="     form-wrapper">
-                            <div class="label-wrapper">
-                                <label class="label-form" for="input-alamat-perusahaan">Alamat Perusahaan</label>
-                            </div>
-                            <div class="input-wrapper">
-                                <textarea name="alamat-perusahaan" class="form-control" id="input-alamat-perusahaan" rows="3">{{ $job->alamat_perusahaan ?? '' }}</textarea>
-                            </div>
-                        </div>
+                        <input type="text" name="divisi" class="form-control" id="input-divisi" value="{{ old('divisi', $job->divisi ?? '') }}"">
+                    </div>
+                </div>
+                <div class="form-wrapper">
+                    <div class="label-wrapper">
+                        <label class="label-form" for="input-jabatan">Jabatan</label>
+                    </div>
+                    <div class="input-wrapper">
+                        <input type="text" name="jabatan" class="form-control" id="input-jabatan" value="{{ old('jabatan', $job->jabatan ?? '') }}"">
+                    </div>
+                </div>
+                <div class="form-wrapper">
+                    <div class="label-wrapper">
+                        <label class="label-form" for="input-alamat-perusahaan">Alamat Perusahaan</label>
+                    </div>
+                    <div class="input-wrapper">
+                        <textarea name="alamat_perusahaan" class="form-control" id="input-alamat-perusahaan" rows="3">{{ old('alamat_perusahaan', $job->alamat_perusahaan ?? '') }}</textarea>
+                    </div>
+                </div>
 
-                        <div class="button-wrapper">
-                            <button type="submit" class="btn btn-primary">Submit</button>
-                        </div>
+                <div class="button-wrapper">
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </div>
             </form>
         </div>
 

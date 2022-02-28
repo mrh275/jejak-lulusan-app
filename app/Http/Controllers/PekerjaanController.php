c81802d054c6bbfceeba6a2cf092c0fbf85b0a99
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2022-02-28 18:00:28 +0700

    created function for insert and update data pekerjaan

--- app/Http/Controllers/PekerjaanController.php
+++ app/Http/Controllers/PekerjaanController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Pekerjaan;
 use Illuminate\Http\Request;
 
 class PekerjaanController extends Controller
@@ -34,7 +35,29 @@ class PekerjaanController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $rules = [
+            'nama_perusahaan' => 'required',
+            'industri'  => 'required',
+            'divisi' => 'required',
+            'jabatan' => 'required',
+            'alamat_perusahaan' => 'required',
+        ];
+
+        $errorMessage = [
+            'nama_perusahaan.required' => 'Nama Perusahaan tidak boleh kosong',
+            'industri.required' => 'Industri tidak boleh kosong',
+            'divisi.required' => 'Divisi tidak boleh kosong',
+            'jabatan.required' => 'Jabatan tidak boleh kosong',
+            'alamat_perusahaan.required' => 'Alamat Perusahaan tidak boleh kosong',
+        ];
+
+        $validatePekerjaan = $request->validate($rules, $errorMessage);
+        $validatePekerjaan['nisn_pekerjaan'] = $request->nisn_pekerjaan;
+        $validatePekerjaan['created_at'] = date('Y-m-d H:i:s');
+
+        Pekerjaan::insert($validatePekerjaan);
+
+        return redirect('alumni/data-pekerjaan')->with('success', 'Data pekerjaan berhasil ditambahkan');
     }
 
     /**
@@ -68,7 +91,27 @@ class PekerjaanController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $rules = [
+            'nama_perusahaan' => 'required',
+            'industri'  => 'required',
+            'divisi' => 'required',
+            'jabatan' => 'required',
+            'alamat_perusahaan' => 'required',
+        ];
+
+        $errorMessage = [
+            'nama_perusahaan.required' => 'Nama Perusahaan tidak boleh kosong',
+            'industri.required' => 'Industri tidak boleh kosong',
+            'divisi.required' => 'Divisi tidak boleh kosong',
+            'jabatan.required' => 'Jabatan tidak boleh kosong',
+            'alamat_perusahaan.required' => 'Alamat Perusahaan tidak boleh kosong',
+        ];
+
+        $validatePekerjaan = $request->validate($rules, $errorMessage);
+
+        Pekerjaan::where('nisn_pekerjaan', $id)->update($validatePekerjaan);
+
+        return redirect('alumni/data-pekerjaan')->with('success', 'Data pekerjaan berhasil diubah');
     }
 
     /**

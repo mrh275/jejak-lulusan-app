0539b893cd00387c92853db501e7f67b6d660f6c
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2022-03-20 14:53:23 +0700

    added modal view component

---
+++ resources/views/component/modal.blade.php
@@ -0,0 +1,121 @@
+<div class="modal modal-show" id="modal" style="z-index: 99">
+    <div class="modal-container">
+      <div class="modal-card card-xl">
+        <button type="button" id="modal-close" class="modal-close">
+          <i class="fa fa-times" aria-hidden="true"></i>
+        </button>
+        <div class="card-header" style="justify-content: start">
+          <h2 style="font-weight: 600; font-size: 1.4rem; color: #334155" >Edit data Siswa</h2>
+        </div>
+        <div class="card-body" style="margin-top: 1rem">
+            <form action="{{ url('/biodata/' . $biodata->nisn) }}" class="form-group" method="post">
+                @method('put')
+                @csrf
+                <div class="form-wrapper">
+                    <div class="label-wrapper">
+                        <label class="label-form" for="input-nama">Nama Lengkap</label>
+                    </div>
+                    <div class="input-wrapper">
+                        <input type="text" name="nama" class="form-control" id="input-nama" value="{{ $biodata->nama ?? '' }}">
+                    </div>
+                </div>
+                <div class="form-wrapper">
+                    <div class="label-wrapper">
+                        <label class="label-form" for="input-nisn">NISN</label>
+                    </div>
+                    <div class="input-wrapper">
+                        <input type="number" name="nisn" class="form-control" id="input-nisn" value="{{ $biodata->nisn ?? '' }}">
+                    </div>
+                </div>
+                <div class="form-wrapper">
+                    <div class="label-wrapper">
+                        <label class="label-form" for="input-nis">NIS</label>
+                    </div>
+                    <div class="input-wrapper">
+                        <input type="number" name="nis" class="form-control" id="input-nis" value="{{ $biodata->nis ?? '' }}">
+                    </div>
+                </div>
+                <div class="form-wrapper">
+                    <div class="label-wrapper">
+                        <label class="label-form" for="input-jenisKelamin">Jenis Kelamin</label>
+                    </div>
+                    <div class="input-wrapper">
+                        <select name="jenis_kelamin" id="input-jenisKelamin" class="form-control">
+                            <option value="">Pilih :</option>
+                            <option value="L" {{ $biodata->jenis_kelamin == 'L' ? 'selected' : '' }}>Laki-laki</option>
+                            <option value="P" {{ $biodata->jenis_kelamin == 'P' ? 'selected' : '' }}>Perempuan</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="form-wrapper">
+                    <div class="label-wrapper">
+                        <label class="label-form" for="input-tempatLahir">Tempat Lahir</label>
+                    </div>
+                    <div class="input-wrapper">
+                        <input type="text" name="tempat_lahir" class="form-control" id="input-tempatLahir" value="{{ $biodata->tempat_lahir ?? '' }}">
+                    </div>
+                </div>
+                <div class="form-wrapper">
+                    <div class="label-wrapper">
+                        <label class="label-form" for="input-tanggalLahir">Tanggal Lahir</label>
+                    </div>
+                    <div class="input-wrapper">
+                        <input type="text" name="tanggal_lahir" class="form-control" id="input-tanggalLahir" value="{{ isset($biodata->tanggal_lahir) ? date('d/m/Y', strtotime($biodata->tanggal_lahir)) : '01/01/1900' }}">
+                    </div>
+                </div>
+                <div class="form-wrapper">
+                    <div class="label-wrapper">
+                        <label class="label-form" for="input-kelas">Kelas</label>
+                    </div>
+                    <div class="input-wrapper">
+                        <select name="kelas" id="input-kelas" class="form-control">
+                            <option value="">Pilih :</option>
+                            <option value="12-ipa-1" {{ $biodata->kelas == '12-ipa-1' ? 'selected' : '' }}>12 IPA 1</option>
+                            <option value="12-ipa-2" {{ $biodata->kelas == '12-ipa-2' ? 'selected' : '' }}>12 IPA 2</option>
+                            <option value="12-ipa-3" {{ $biodata->kelas == '12-ipa-3' ? 'selected' : '' }}>12 IPA 3</option>
+                            <option value="12-ipa-4" {{ $biodata->kelas == '12-ipa-4' ? 'selected' : '' }}>12 IPA 4</option>
+                            <option value="12-ips-1" {{ $biodata->kelas == '12-ips-1' ? 'selected' : '' }}>12 IPS 1</option>
+                            <option value="12-ips-2" {{ $biodata->kelas == '12-ips-2' ? 'selected' : '' }}>12 IPS 2</option>
+                            <option value="12-ips-3" {{ $biodata->kelas == '12-ips-3' ? 'selected' : '' }}>12 IPS 3</option>
+                            <option value="12-ips-4" {{ $biodata->kelas == '12-ips-4' ? 'selected' : '' }}>12 IPS 4</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="form-wrapper">
+                    <div class="label-wrapper">
+                        <label class="label-form" for="input-tahunLulus">Tahun Lulus</label>
+                    </div>
+                    <div class="input-wrapper">
+                        <input type="text" name="tahun_lulus" class="form-control" id="input-tahunLulus" value="{{ $biodata->tahun_lulus ?? '' }}">
+                    </div>
+                </div>
+                <div class="form-wrapper">
+                    <div class="label-wrapper">
+                        <label class="label-form" for="input-statusLulusan">Status Lulus</label>
+                    </div>
+                    <div class="input-wrapper">
+                        <select name="status_lulusan" id="input_statusLulusan" class="form-control">
+                            <option value="">Pilih :</option>
+                            <option value="1" {{ $biodata->status_lulusan == 1 ? 'selected' : '' }}>Kuliah</option>
+                            <option value="2" {{ $biodata->status_lulusan == 2 ? 'selected' : '' }}>Bekerja</option>
+                            <option value="3" {{ $biodata->status_lulusan == 3 ? 'selected' : '' }}>Kuliah dan Kerja</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="form-wrapper">
+                    <div class="label-wrapper">
+                        <label class="label-form" for="input-alamat">Alamat</label>
+                    </div>
+                    <div class="input-wrapper">
+                        <textarea name="alamat" class="form-control" id="input-alamat" rows="3">{{ $biodata->alamat ?? '' }}</textarea>
+                    </div>
+                </div>
+
+                <div class="button-wrapper">
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </div>
+            </form>
+        </div>
+      </div>
+    </div>
+  </div>

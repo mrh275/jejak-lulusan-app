1cf76166f023f414914e8135c257e145766d8959
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2022-03-20 10:52:21 +0700

    modified page number type

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,26 +1,24 @@
-
-
 // Toggle Menu
-document.getElementById("toggleMenu").addEventListener("click", function() {
-  document.getElementById("menu").classList.toggle("-left-full");
-  document.getElementById("menu").classList.toggle("left-0");
+document.getElementById("toggleMenu").addEventListener("click", function () {
+    document.getElementById("menu").classList.toggle("-left-full");
+    document.getElementById("menu").classList.toggle("left-0");
 
-  let toggleIcon = document.getElementById("toggleMenu").children;
-  toggleIcon[0].classList.toggle("change-1");
-  toggleIcon[1].classList.toggle("change-2");
-  toggleIcon[2].classList.toggle("change-3");
+    let toggleIcon = document.getElementById("toggleMenu").children;
+    toggleIcon[0].classList.toggle("change-1");
+    toggleIcon[1].classList.toggle("change-2");
+    toggleIcon[2].classList.toggle("change-3");
+});
 
-})
-
-document.querySelector('.dropdown').addEventListener('click', function() {
-  document.querySelector('.dropdown-menu').classList.toggle('hide');
-  document.querySelector('.dropdown-menu').classList.toggle('show');
-})
+document.querySelector(".dropdown").addEventListener("click", function () {
+    document.querySelector(".dropdown-menu").classList.toggle("hide");
+    document.querySelector(".dropdown-menu").classList.toggle("show");
+});
 
 // Datatables
-$(document).ready(function() {
-  $('#daftar-lulusan').DataTable({
-    "autoWidth": false,
-    "scrollX": true,
-  });
-} );
+$(document).ready(function () {
+    let dataLulusan = $("#daftar-lulusan").DataTable({
+        autoWidth: false,
+        scrollX: true,
+        pagingType: "full_numbers",
+    });
+});

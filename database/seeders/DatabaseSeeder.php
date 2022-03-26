b47bce7b3f17b49804844fa036477fe961bccbbc
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2022-03-27 01:14:18 +0700

    added seeders for tahun lulus

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,11 +2,13 @@
 
 namespace Database\Seeders;
 
-use App\Models\Biodata;
-use App\Models\DataOrangTua;
-use App\Models\Kuliah;
-use App\Models\Pekerjaan;
 use App\Models\User;
+use App\Models\Kuliah;
+use App\Models\Biodata;
+use App\Models\Angkatan;
+use App\Models\Pekerjaan;
+use App\Models\DataOrangTua;
+use App\Models\Kelas;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 
@@ -39,6 +41,9 @@ class DatabaseSeeder extends Seeder
         // Biodata::factory(50)->create();
         // Kuliah::factory(38)->create();
         // Pekerjaan::factory(33)->create();
-        DataOrangTua::factory(50)->create();
+        // DataOrangTua::factory(50)->create();
+
+        Angkatan::factory(4)->create();
+        // Kelas::factory(5)->create();
     }
 }

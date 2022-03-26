78a57e760026cd8df23623913df9b822d42fe1bc
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2022-03-27 01:13:19 +0700

    added new method for export data angkatan

--- app/Http/Controllers/ExportLulusanController.php
+++ app/Http/Controllers/ExportLulusanController.php
@@ -2,9 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Exports\ExportAll;
 use Illuminate\Http\Request;
+use App\Exports\ExportAngkatan;
+use Maatwebsite\Excel\Facades\Excel;
 
 class ExportLulusanController extends Controller
 {
-    //
+    public function exportExcel() {
+        return Excel::download(new ExportAll, 'data-lulusan-all.xlsx');
+    }
+
+    public function exportAngkatan(Request $request) {
+        $angkatan = $request->angkatan;
+        return Excel::download(new ExportAngkatan($angkatan), 'data-lulusan-angkatan-'.$angkatan.'.xlsx');
+    }
 }

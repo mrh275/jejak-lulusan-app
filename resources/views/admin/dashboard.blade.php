f5bd3813f6fabb7d30eb5a27c36bebfb74e8be91
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2022-03-27 23:43:49 +0700

    modified dashboard element

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -7,12 +7,12 @@
         <div class="header">
             <div class="card post">
                 <div class="card-body">
-                    <span class="count-number">50</span>
-                    <h2 class="header-title">Post</h2>
-                    <i class='bx bxs-edit'></i>
+                    <span class="count-number">{{ count($alumni) }}</span>
+                    <h2 class="header-title">Alumni</h2>
+                    <i class='bx bxs-graduation'></i>
                 </div>
                 <div class="card-footer">
-                    <a href="../page/list-post.html" class="header-readMore">
+                    <a href="{{ url('admin/datatable-biodata') }}" class="header-readMore">
                         <span>See more</span>
                         <i class='bx bx-right-arrow-alt'></i>
                     </a>
@@ -20,12 +20,12 @@
             </div>
             <div class="card comment">
                 <div class="card-body">
-                    <span class="count-number">823</span>
-                    <h2 class="header-title">Comment</h2>
-                    <i class='bx bxs-comment-detail'></i>
+                    <span class="count-number">{{ $alumni->where('status_lulusan', 1)->count() + $alumni->where('status_lulusan', 3)->count() }}</span>
+                    <h2 class="header-title">Kuliah</h2>
+                    <i class='bx bxs-school'></i>
                 </div>
                 <div class="card-footer">
-                    <a href="../page/comment.html" class="header-readMore">
+                    <a href="{{ url('admin/datatable-kuliah') }}" class="header-readMore">
                         <span>See more</span>
                         <i class='bx bx-right-arrow-alt'></i>
                     </a>
@@ -33,16 +33,24 @@
             </div>
             <div class="card visitor">
                 <div class="card-body">
-                    <span class="count-number">999</span>
-                    <h2 class="header-title">Visitor</h2>
-                    <i class='bx bx-line-chart'></i>
+                    <span class="count-number">{{ $alumni->where('status_lulusan', 2)->count() + $alumni->where('status_lulusan', 3)->count() }}</span>
+                    <h2 class="header-title">Kerja</h2>
+                    <i class='bx bxs-briefcase'></i>
+                </div>
+                <div class="card-footer">
+                    <a href="{{ url('admin/datatable-pekerjaan') }}" class="header-readMore">
+                        <span>See more</span>
+                        <i class='bx bx-right-arrow-alt'></i>
+                    </a>
                 </div>
             </div>
             <div class="card user">
                 <div class="card-body">
-                    <span class="count-number">50</span>
-                    <h2 class="header-title">Post</h2>
-                    <i class='bx bxs-edit'></i>
+                    <span class="count-number">
+                        {{ $angkatan }}
+                    </span>
+                    <h2 class="header-title">Angkatan</h2>
+                    <i class='bx bxs-coin-stack'></i>
                 </div>
                 <div class="card-footer">
                     <a href="../page/list-post.html" class="header-readMore">
@@ -59,91 +67,105 @@
                     <h3 class="popular-post-title">Popular Post</h3>
                 </div>
                 <div class="popular-post-body">
-                    <div class="popular-post-card">
-                        <div class="card-img">
-                            <img src="{{ url('assets/img') }}/img-1.jpg" alt="Images 1" class="popular-post-img">
-                        </div>
-                        <div class="card-text">
-                            <h4 class="post-title">Judul Postingan 1</h4>
-                            <span class="popular-post-sub">
-                                <i class='bx bxs-user'></i> <span class="author">Penulis</span>
-                                <i class='bx bxs-calendar'></i> <span class="date-posted">21 Oktober 2021</span>
-                                <i class="fa fa-eye"></i>
-                                <spam class="views">999 Views</spam>
-                            </span>
-
-                        </div>
-                    </div>
-
-                    <div class="popular-post-card">
-                        <div class="card-img">
-                            <img src="{{ url('assets/img') }}/img-2.jpg" alt="Images 2" class="popular-post-img">
-                        </div>
-                        <div class="card-text">
-                            <h4 class="post-title">Judul Postingan 2</h4>
-                            <span class="popular-post-sub">
-                                <i class='bx bxs-user'></i> <span class="author">Penulis</span>
-                                <i class='bx bxs-calendar'></i> <span class="date-posted">21 Oktober 2021</span>
-                                <i class="fa fa-eye"></i>
-                                <spam class="views">999 Views</spam>
-                            </span>
-
-                        </div>
-                    </div>
-
-                    <div class="popular-post-card">
-                        <div class="card-img">
-                            <img src="{{ url('assets/img') }}/img-3.jpg" alt="Images 3" class="popular-post-img">
-                        </div>
-                        <div class="card-text">
-                            <h4 class="post-title">Judul Postingan 3</h4>
-                            <span class="popular-post-sub">
-                                <i class='bx bxs-user'></i> <span class="author">Penulis</span>
-                                <i class='bx bxs-calendar'></i> <span class="date-posted">21 Oktober 2021</span>
-                                <i class="fa fa-eye"></i>
-                                <spam class="views">999 Views</spam>
-                            </span>
-
-                        </div>
-                    </div>
-
-                    <div class="popular-post-card">
-                        <div class="card-img">
-                            <img src="{{ url('assets/img') }}/img-4.jpg" alt="Images 4" class="popular-post-img">
-                        </div>
-                        <div class="card-text">
-                            <h4 class="post-title">Judul Postingan 4</h4>
-                            <span class="popular-post-sub">
-                                <i class='bx bxs-user'></i> <span class="author">Penulis</span>
-                                <i class='bx bxs-calendar'></i> <span class="date-posted">21 Oktober 2021</span>
-                                <i class="fa fa-eye"></i>
-                                <spam class="views">999 Views</spam>
-                            </span>
-
-                        </div>
-                    </div>
 
                 </div>
             </div>
             <div class="visitor-chart stat-card">
                 <div class="visitor-chart-header">
-                    <h4 class="visitor-title">Visitor Statistics</h4>
+                    <h4 class="visitor-title">Statistik Alumni</h4>
                 </div>
-                <!-- <div class="visitor-chart-body">
-                                <h3 class="total-visitor">999 visitor</h3>
-                                <canvas id="visitor-chart"></canvas>
-                            </div> -->
+                <div class="visitor-chart-body">
+                    <h3 class="total-visitor">{{ $alumni->count() }} alumni</h3>
+                    <canvas id="alumni-chart"></canvas>
+                </div>
             </div>
         </div>
 
-        <div class="second-container">
+        {{-- <div class="second-container">
             <div class="stat-card">
 
             </div>
             <div class="stat-card">
 
             </div>
-        </div>
+        </div> --}}
 
     </div>
 @endsection
+
+@push('scripts')
+    <script>
+        const labels = [
+            2014,
+            2015,
+            2016,
+            2017,
+            2018,
+            2019,
+            2020,
+            2021,
+        ];
+
+        const data = {
+            labels: labels,
+            datasets: [
+                {
+                label: "{{ $alumni->where('status_lulusan', 1)->count() }} Kuliah",
+                backgroundColor: 'rgba(255, 99, 132, 0.5)',
+                borderColor: 'rgba(255, 99, 132, 1)',
+                data: [
+                            {{ $alumni->where('tahun_lulus', 2014)->where('status_lulusan', 1)->count() }},
+                            {{ $alumni->where('tahun_lulus', 2015)->where('status_lulusan', 1)->count() }},
+                            {{ $alumni->where('tahun_lulus', 2016)->where('status_lulusan', 1)->count() }},
+                            {{ $alumni->where('tahun_lulus', 2017)->where('status_lulusan', 1)->count() }},
+                            {{ $alumni->where('tahun_lulus', 2018)->where('status_lulusan', 1)->count() }},
+                            {{ $alumni->where('tahun_lulus', 2019)->where('status_lulusan', 1)->count() }},
+                            {{ $alumni->where('tahun_lulus', 2020)->where('status_lulusan', 1)->count() }},
+                            {{ $alumni->where('tahun_lulus', 2021)->where('status_lulusan', 1)->count() }},
+                        ],
+                },
+                {
+                label: "{{ $alumni->where('status_lulusan', 2)->count() }} Kerja",
+                backgroundColor: 'rgba(00, 99, 255, 0.5)',
+                borderColor: 'rgba(00, 99, 255, 1)',
+                data: [
+                        {{ $alumni->where('tahun_lulus', 2014)->where('status_lulusan', 2)->count() }},
+                        {{ $alumni->where('tahun_lulus', 2015)->where('status_lulusan', 2)->count() }}, 
+                        {{ $alumni->where('tahun_lulus', 2016)->where('status_lulusan', 2)->count() }}, 
+                        {{ $alumni->where('tahun_lulus', 2017)->where('status_lulusan', 2)->count() }},
+                        {{ $alumni->where('tahun_lulus', 2018)->where('status_lulusan', 2)->count() }},
+                        {{ $alumni->where('tahun_lulus', 2019)->where('status_lulusan', 2)->count() }},
+                        {{ $alumni->where('tahun_lulus', 2020)->where('status_lulusan', 2)->count() }},
+                        {{ $alumni->where('tahun_lulus', 2021)->where('status_lulusan', 2)->count() }},
+                    ],
+                },
+                {
+                label: "{{ $alumni->where('status_lulusan', 3)->count() }} Kuliah dan Kerja",
+                backgroundColor: 'rgba(245,158,11,0.5)',
+                borderColor: 'rgba(245,158,11,1)',
+                data: [
+                        {{ $alumni->where('tahun_lulus', 2014)->where('status_lulusan', 3)->count() }},
+                        {{ $alumni->where('tahun_lulus', 2015)->where('status_lulusan', 3)->count() }}, 
+                        {{ $alumni->where('tahun_lulus', 2016)->where('status_lulusan', 3)->count() }}, 
+                        {{ $alumni->where('tahun_lulus', 2017)->where('status_lulusan', 3)->count() }},
+                        {{ $alumni->where('tahun_lulus', 2018)->where('status_lulusan', 3)->count() }},
+                        {{ $alumni->where('tahun_lulus', 2019)->where('status_lulusan', 3)->count() }},
+                        {{ $alumni->where('tahun_lulus', 2020)->where('status_lulusan', 3)->count() }},
+                        {{ $alumni->where('tahun_lulus', 2021)->where('status_lulusan', 3)->count() }},
+                    ],
+                },
+            ]
+        };
+
+        const config = {
+            type: 'bar',
+            data: data,
+            options: {}
+        };
+
+        const myChart = new Chart(
+            document.getElementById('alumni-chart'),
+            config
+        );
+    </script>
+@endpush    

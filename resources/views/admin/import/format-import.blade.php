3e5f3965d7ff458c83bff73835d7bad7decfbd23
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2022-03-27 20:21:39 +0700

    created view for import data format

---
+++ resources/views/admin/import/format-import.blade.php
@@ -0,0 +1,76 @@
+<table>
+    <thead>
+        <tr>
+            <td>nisn</td>
+            <td>nis</td>
+            <td>nama</td>
+            <td>jenis_kelamin</td>
+            <td>tempat_lahir</td>
+            <td>tanggal_lahir</td>
+            <td>kelas</td>
+            <td>tahun_lulus</td>
+            <td>status_lulusan</td>
+            <td>alamat</td>
+            <td>nama_ayah</td>
+            <td>tempat_lahir_ayah</td>
+            <td>tanggal_lahir_ayah</td>
+            <td>pendidikan_ayah</td>
+            <td>pekerjaan_ayah</td>
+            <td>penghasilan_ayah</td>
+            <td>alamat_ayah</td>
+            <td>nama_ibu</td>
+            <td>tempat_lahir_ibu</td>
+            <td>tanggal_lahir_ibu</td>
+            <td>pendidikan_ibu</td>
+            <td>pekerjaan_ibu</td>
+            <td>penghasilan_ibu</td>
+            <td>alamat_ibu</td>
+            <td>nama_kampus</td>
+            <td>fakultas</td>
+            <td>jurusan</td>
+            <td>alamat_kampus</td>
+            <td>nama_perusahaan</td>
+            <td>industri</td>
+            <td>divisi</td>
+            <td>jabatan</td>
+            <td>alamat_perusahaan</td>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>Silahkan isi disini</td>
+            <td>Silahkan isi disini</td>
+            <td>Silahkan isi disini</td>
+            <td>Silahkan isi disini</td>
+            <td>Silahkan isi disini</td>
+            <td>Silahkan isi disini</td>
+            <td>Silahkan isi disini</td>
+            <td>Silahkan isi disini</td>
+            <td>Silahkan isi disini</td>
+            <td>Silahkan isi disini</td>
+            <td>Silahkan isi disini</td>
+            <td>Silahkan isi disini</td>
+            <td>Silahkan isi disini</td>
+            <td>Silahkan isi disini</td>
+            <td>Silahkan isi disini</td>
+            <td>Silahkan isi disini</td>
+            <td>Silahkan isi disini</td>
+            <td>Silahkan isi disini</td>
+            <td>Silahkan isi disini</td>
+            <td>Silahkan isi disini</td>
+            <td>Silahkan isi disini</td>
+            <td>Silahkan isi disini</td>
+            <td>Silahkan isi disini</td>
+            <td>Silahkan isi disini</td>
+            <td>Silahkan isi disini</td>
+            <td>Silahkan isi disini</td>
+            <td>Silahkan isi disini</td>
+            <td>Silahkan isi disini</td>
+            <td>Silahkan isi disini</td>
+            <td>Silahkan isi disini</td>
+            <td>Silahkan isi disini</td>
+            <td>Silahkan isi disini</td>
+            <td>Silahkan isi disini</td>
+        </tr>
+    </tbody>
+</table>

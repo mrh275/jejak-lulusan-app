7a2a6679ad48c2aca27badb49c38151d6d696619
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2022-03-27 20:18:34 +0700

    added method for insert import data

--- app/Http/Controllers/ExportLulusanController.php
+++ app/Http/Controllers/ExportLulusanController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Exports\ExportAll;
 use Illuminate\Http\Request;
 use App\Exports\ExportAngkatan;
+use App\Imports\ImportLulusan;
 use Maatwebsite\Excel\Facades\Excel;
 
 class ExportLulusanController extends Controller
@@ -16,5 +17,13 @@ class ExportLulusanController extends Controller
     public function exportAngkatan(Request $request) {
         $angkatan = $request->angkatan;
         return Excel::download(new ExportAngkatan($angkatan), 'data-lulusan-angkatan-'.$angkatan.'.xlsx');
+    }
+
+    public function importLulusan(Request $request) {
+        $file = $request->file('file');
+
+        Excel::import(new ImportLulusan, $file);
+
+        return redirect('admin/import-lulusan')->with('success', 'Data berhasil diimport!');
     }
 }

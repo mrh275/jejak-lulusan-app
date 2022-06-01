fdea5edf82a15c178cb0265c4a3acb0175c8cb86
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2022-06-01 21:33:40 +0700

    added kelulusan controller

---
+++ app/Http/Controllers/KelulusanController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Biodata;
+use App\Models\DataOrangTua;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class KelulusanController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $response = [
+            'message' => 'Data ditemukan',
+            'biodata' => Biodata::all(),
+        ];
+
+        return response()->json($response, Response::HTTP_OK);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $biodata = Biodata::where('nisn', $id)->first();
+        $dataOrangTua = DataOrangTua::where('nisn_orang_tua', $id)->first();
+
+        $response = [
+            'message' => 'Data ditemukan',
+            'biodata' => $biodata,
+            'dataOrangTua' => $dataOrangTua
+        ];
+
+        return response()->json($response, Response::HTTP_OK);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
